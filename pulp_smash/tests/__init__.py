bd5824c48326e8d8e72ff203d3b0b2e16d652894
Jeremy Audet <ichimonji10@gmail.com>
2015-10-12 14:36:49 -0400

    Ignore SSL verification warnings
    Fix #4:
    
    > Let's suppress SSL verification warnings. If a user sets "verify": False in
    > their configuration file, then the responsibility for the fall-out of that
    > decision lies with them. And if they do not suppress SSL verification, then a
    > different warning is raised.
    
    Do this by using `warnings.simplefilter`. Example results:
    
        $ python -m unittest2 discover pulp_smash.tests
        ....F...
        ======================================================================
        FAIL: test_body (pulp_smash.tests.test_login.LoginFailureTestCase)
        Assert that the response is valid JSON and does not have "key" and
        ----------------------------------------------------------------------
        Traceback (most recent call last):
          File "/home/ichimonji10/code/pulp-smash/pulp_smash/tests/test_login.py", line 70, in test_body
            self.assertEqual(set(self.response.json().keys()), ERROR_KEYS)
        AssertionError: Items in the first set but not the second:
        'auth_error_code'
        'http_request_method'
        Items in the second set but not the first:
        'href'
    
        ----------------------------------------------------------------------
        Ran 8 tests in 4.857s
    
        FAILED (failures=1)
    
    The test failure is known about and should be present. See #9.

--- pulp_smash/tests/__init__.py
+++ pulp_smash/tests/__init__.py
@@ -7,5 +7,40 @@ interfaces exposed by Pulp, including its API and CLI.
 
 These tests are entirely different from the tests in :mod:`tests`.
 
+-----
+
+By default, Requests refuses to make insecure HTTPS connections. You can
+override this behaviour by passing ``verify=False``. For example::
+
+    requests.get('https://insecure.example.com', verify=False)
+
+If you do this, Requests will make the connection. However, an
+``InsecureRequestWarning`` is still raised. This is problematic. If a user has
+explicitly stated that they want insecure HTTPS connections and they are
+pestered about that fact, the user is effectively trained to ignore warnings.
+
+Thus, when this module is imported, it suppresses
+``requests.packages.urllib3.exceptions.InsecureRequestWarning``. This filtering
+does not affect whether SSL verification is performed. If an insecure
+connection is attempted and ``verify=True`` or is unspecified, the connection
+is not made.
+
+.. NOTE:: The ``InsecureRequestWarning`` suppression is process-wide.
+
+Unfortunately, Python's warnings and filter system is process-wide. Imagine an
+application which uses both Pulp Smash and some other library, and imagine that
+the other library generates ``InsecureRequestWarning`` warnings. The warnings
+raised by that application are suppressed by the filter created here. The
+``warnings.catch_warnings`` context manager is not a good solution to this
+problem, as it is thread-unsafe.
+
 """
 from __future__ import unicode_literals
+
+import requests
+from warnings import simplefilter
+
+simplefilter(
+    'ignore',
+    requests.packages.urllib3.exceptions.InsecureRequestWarning,
+)
