e261a40b2ba923c8706d3abb9e569f86dd7e1f96
Jeremy Audet <ichimonji10@gmail.com>
2015-10-12 14:35:15 -0400

    Document several modules
    Document the following modules:
    
    * `pulp_smash.tests.__init__`
    * `tests.__init__`
    
    Explain that the former contains tests for Pulp, and the latter contains tests
    for Pulp Smash itself.

--- pulp_smash/tests/__init__.py
+++ pulp_smash/tests/__init__.py
@@ -1,3 +1,11 @@
 # coding=utf-8
-"""Integration tests for Pulp."""
+"""Tests for Pulp.
+
+This package and its sub-packages contain tests for Pulp. These tests should be
+run against live Pulp systems. These tests may target all of the different
+interfaces exposed by Pulp, including its API and CLI.
+
+These tests are entirely different from the tests in :mod:`tests`.
+
+"""
 from __future__ import unicode_literals
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,10 @@
 # coding=utf-8
-"""Unit tests for Pulp Smash's internal libraries."""
+"""Unit tests for Pulp Smash.
+
+This package and its sub-packages contain tests for Pulp Smash. These tests
+verify that Pulp Smash's internal functions and libraries function correctly.
+
+These tests are entirely different from the tests in :mod:`pulp_smash.tests`.
+
+"""
 from __future__ import unicode_literals
