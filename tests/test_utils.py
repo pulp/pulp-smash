1529fbe3b9fc538d6585421a350d17d47c635168
Jeremy Audet <ichimonji10@gmail.com>
2015-12-06 13:07:26 -0500

    Add unit tests for generic helper functions
    The newly tested functions are:
    
    * `pulp_smash.utils.create_repository`
    * `pulp_smash.utils.create_user`
    * `pulp_smash.utils.delete`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,10 @@
 """Unit tests for :mod:`pulp_smash.utils`."""
 from __future__ import unicode_literals
 
+import mock
+import requests
 from pulp_smash import utils
+from pulp_smash.config import ServerConfig
 from unittest2 import TestCase
 
 
@@ -12,3 +15,89 @@ class UUID4TestCase(TestCase):
     def test_type(self):
         """Assert the method returns a unicode string."""
         self.assertIsInstance(utils.uuid4(), type(''))
+
+
+class CommonAssertionsMixin(object):
+    """A common set of assertions for generic helper functions.
+
+    Some helper functions have a signature of ``func(server_config, …,
+    responses)``. This class provides test methods that can be used with any of
+    those functions. These methods only work if the following attributes can be
+    accessed:
+
+    =================================  ========================================
+    attribute                          purpose
+    =================================  ========================================
+    ``self.mocks['request']``          A mock object stubbing one of the
+                                       functions in the ``requests`` module.
+    ``self.mocks['handle_response']``  A mock object stubbing the
+                                       ``pulp_smash.utils.handle_response``
+                                       function.
+    ``self.output``                    The value returned by the function under
+                                       test.
+    =================================  ========================================
+
+    """
+
+    def test_requests_called(self):
+        """Assert the Requests function is called once."""
+        self.assertEqual(self.mocks['request'].call_count, 1)
+
+    def test_handle_response_called(self):
+        """Assert ``handle_response`` is called once."""
+        self.assertEqual(self.mocks['handle_response'].call_count, 1)
+
+    def test_return_value(self):
+        """Assert that whatever ``handle_response`` returns is returned."""
+        self.assertIs(self.mocks['handle_response'].return_value, self.output)
+
+
+class CreateRepositoryTestCase(CommonAssertionsMixin, TestCase):
+    """Test :meth:`pulp_smash.utils.create_repository`."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Mock out dependencies and call the function under test."""
+        inputs = {
+            'server_config': ServerConfig('http://example.com'),
+            'body': None,
+            'responses': None,
+        }
+        with mock.patch.object(utils, 'handle_response') as hand_resp:
+            with mock.patch.object(requests, 'post') as request:
+                cls.output = utils.create_repository(**inputs)
+        cls.mocks = {'handle_response': hand_resp, 'request': request}
+
+
+class CreateUserTestCase(CommonAssertionsMixin, TestCase):
+    """Test :meth:`pulp_smash.utils.create_user`."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Mock out dependencies and call the function under test."""
+        inputs = {
+            'server_config': ServerConfig('http://example.com'),
+            'body': None,
+            'responses': None,
+        }
+        with mock.patch.object(utils, 'handle_response') as hand_resp:
+            with mock.patch.object(requests, 'post') as request:
+                cls.output = utils.create_user(**inputs)
+        cls.mocks = {'handle_response': hand_resp, 'request': request}
+
+
+class DeleteTestCase(CommonAssertionsMixin, TestCase):
+    """Test :meth:`pulp_smash.utils.delete`."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Mock out dependencies and call the function under test."""
+        inputs = {
+            'server_config': ServerConfig('http://example.com'),
+            'href': utils.uuid4(),
+            'responses': None,
+        }
+        with mock.patch.object(utils, 'handle_response') as hand_resp:
+            with mock.patch.object(requests, 'delete') as request:
+                cls.output = utils.delete(**inputs)
+        cls.mocks = {'handle_response': hand_resp, 'request': request}
