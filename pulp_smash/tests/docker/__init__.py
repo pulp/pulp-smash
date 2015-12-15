766b4c9bbe26875c8ea8b6e477fdd9c8ce5b0033
Randy Barlow <rbarlow@redhat.com>
2015-12-15 14:34:43 -0500

    Add some tests for Docker repository C.
    (The RUD will be added later…)

---
+++ pulp_smash/tests/docker/__init__.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+"""This package contains tests for the Docker plugin."""
+from __future__ import unicode_literals
---
+++ pulp_smash/tests/docker/api_v2/__init__.py
@@ -0,0 +1,1 @@
+"""This package contains tests for the Docker plugin with the Pulp v2 API."""
