98699104102f91e5feb8b1cb99dff57b7b19e73d
Jeremy Audet <ichimonji10@gmail.com>
2016-01-11 12:31:16 -0500

    Refactor repository API tests
    Refactor module `pulp_smash.tests.platform.api_v2.test_repository`. Make this
    module use the new `pulp_smash.api.Client` API client. Doing this makes the
    module more readable and concise. Test suite results do not change (13 tests):
    
        ============  =============================
        Pulp Version  Test Suite Results
        ============  =============================
        2.7           OK (skipped=6)
        dev (2.8)     OK (skipped=6)
        ============  =============================
    
    Results obtained with the following command:
    
        PULP_SMASH_CONFIG_FILE=$PS_CONFIG \
        python -m unittest2 pulp_smash.tests.platform.api_v2.test_repository
    
    Related to #64.

--- pulp_smash/tests/platform/api_v2/test_repository.py
+++ pulp_smash/tests/platform/api_v2/test_repository.py
@@ -21,18 +21,32 @@ try:  # try Python 3 first
 except ImportError:
     from urlparse import urljoin  # pylint:disable=C0411,E0401
 
-import requests
-from requests.exceptions import HTTPError
-from unittest2 import TestCase
+import unittest2
 
-from pulp_smash.config import get_config
+from pulp_smash import api, config, utils
 from pulp_smash.constants import REPOSITORY_PATH, ERROR_KEYS
 from pulp_smash.selectors import bug_is_untestable, require
-from pulp_smash.utils import create_repository, delete, uuid4
 
 
-class CreateSuccessTestCase(TestCase):
-    """Establish that we can create repositories."""
+class _BaseTestCase(unittest2.TestCase):
+    """Provide a server config to tests, and delete created resources."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Provide a server config and an empty set of resources to delete."""
+        cls.cfg = config.get_config()
+        cls.resources = set()  # a set of _href paths
+
+    @classmethod
+    def tearDownClass(cls):
+        """For each resource in ``cls.resources``, delete that resource."""
+        client = api.Client(cls.cfg)
+        for resource in cls.resources:
+            client.delete(resource)
+
+
+class CreateSuccessTestCase(_BaseTestCase):
+    """Establish we can create repositories."""
 
     @classmethod
     def setUpClass(cls):
@@ -41,52 +55,47 @@ class CreateSuccessTestCase(TestCase):
         Create one repository with the minimum required attributes, and a
         second with all available attributes except importers and distributors.
         """
-        cls.cfg = get_config()
-        cls.bodies = (
-            {'id': uuid4()},
-            {key: uuid4() for key in ('id', 'display_name', 'description')},
-        )
-        cls.bodies[1]['notes'] = {uuid4(): uuid4()}
+        super(CreateSuccessTestCase, cls).setUpClass()
+        client = api.Client(cls.cfg)
+        cls.bodies = [{'id': utils.uuid4()}]
+        cls.bodies.append({
+            'description': utils.uuid4(),
+            'display_name': utils.uuid4(),
+            'id': utils.uuid4(),
+            'notes': {utils.uuid4(): utils.uuid4()},
+        })
         cls.responses = []
-        cls.attrs_iter = tuple((  # 1:1 correlation with cls.bodies
-            create_repository(cls.cfg, body, cls.responses)
-            for body in cls.bodies
-        ))
+        for body in cls.bodies:
+            response = client.post(REPOSITORY_PATH, body)
+            cls.responses.append(response)
+            cls.resources.add(response.json()['_href'])
 
     def test_status_code(self):
-        """Assert that each response has a HTTP 201 status code."""
-        for i, response in enumerate(self.responses):
-            with self.subTest(self.bodies[i]):
+        """Assert each response has an HTTP 201 status code."""
+        for body, response in zip(self.bodies, self.responses):
+            with self.subTest(body=body):
                 self.assertEqual(response.status_code, 201)
 
     @require('2.7')  # https://pulp.plan.io/issues/695
     def test_location_header(self):
-        """Assert that the Location header is correctly set in the response."""
-        for response, attrs in zip(self.responses, self.attrs_iter):
-            with self.subTest((response, attrs)):
-                url = '{}{}{}/'.format(
-                    self.cfg.base_url,
-                    REPOSITORY_PATH,
-                    attrs['id'],
-                )
+        """Assert the Location header is correctly set in each response."""
+        for body, response in zip(self.bodies, self.responses):
+            with self.subTest(body=body):
+                url = urljoin(self.cfg.base_url, REPOSITORY_PATH)
+                url = urljoin(url, body['id']) + '/'
                 self.assertEqual(response.headers['Location'], url)
 
     def test_attributes(self):
         """Assert that each repository has the requested attributes."""
-        for body, attrs in zip(self.bodies, self.attrs_iter):
-            with self.subTest((body, attrs)):
+        for body, response in zip(self.bodies, self.responses):
+            with self.subTest(body=body):
+                attrs = response.json()
                 self.assertLessEqual(set(body.keys()), set(attrs.keys()))
                 attrs = {key: attrs[key] for key in body.keys()}
                 self.assertEqual(body, attrs)
 
-    @classmethod
-    def tearDownClass(cls):
-        """Delete the created repositories."""
-        for attrs in cls.attrs_iter:
-            delete(cls.cfg, attrs['_href'])
 
-
-class CreateFailureTestCase(TestCase):
+class CreateFailureTestCase(_BaseTestCase):
     """Establish that repositories are not created in documented scenarios."""
 
     @classmethod
@@ -96,21 +105,22 @@ class CreateFailureTestCase(TestCase):
         Each repository is created to test a different failure scenario. The
         first repository is created in order to test duplicate ids.
         """
-        cls.cfg = get_config()
-        cls.attrs_iter = (create_repository(cls.cfg, {'id': uuid4()}),)
+        super(CreateFailureTestCase, cls).setUpClass()
+        client = api.Client(cls.cfg, api.json_handler)
+        repo = client.post(REPOSITORY_PATH, {'id': utils.uuid4()})
+        cls.resources.add(repo['_href'])
+
+        client.response_handler = api.echo_handler
         cls.bodies = (
             {'id': None},  # 400
             ['Incorrect data type'],  # 400
             {'missing_required_keys': 'id'},  # 400
-            {'id': cls.attrs_iter[0]['id']},  # 409
+            {'id': repo['id']},  # 409
         )
         cls.status_codes = (400, 400, 400, 409)
-        cls.responses = []
-        for body in cls.bodies:
-            try:
-                create_repository(cls.cfg, body, cls.responses)
-            except HTTPError:
-                pass
+        cls.responses = [
+            client.post(REPOSITORY_PATH, body) for body in cls.bodies
+        ]
 
     def test_status_code(self):
         """Assert that each response has the expected HTTP status code."""
@@ -142,68 +152,46 @@ class CreateFailureTestCase(TestCase):
             with self.subTest(body=body):
                 if bug_is_untestable(1413):  # fails for all bodies
                     self.skipTest('https://pulp.plan.io/issues/1413')
-                self.assertEqual(
-                    frozenset(response.json().keys()),
-                    ERROR_KEYS,
-                )
-
-    @classmethod
-    def tearDownClass(cls):
-        """Delete the created repositories."""
-        for attrs in cls.attrs_iter:
-            delete(cls.cfg, attrs['_href'])
+                response_keys = frozenset(response.json().keys())
+                self.assertEqual(response_keys, ERROR_KEYS)
 
 
-class ReadUpdateDeleteSuccessTestCase(TestCase):
-    """Establish that we can read, update, and delete repositories.
+class ReadUpdateDeleteTestCase(_BaseTestCase):
+    """Establish we can read, update and delete repositories.
 
-    This test assumes that the assertions in :class:`CreateSuccessTestCase` are
-    valid.
+    This test case assumes the assertions in :class:`CreateSuccessTestCase`
+    hold true.
     """
 
     @classmethod
     def setUpClass(cls):
-        """Create three repositories and read, update, and delete them."""
-        cls.cfg = get_config()
-        cls.attrs_iter = tuple((
-            create_repository(cls.cfg, {'id': uuid4()}) for _ in range(3)
+        """Create three repositories and read, update and delete them."""
+        super(ReadUpdateDeleteTestCase, cls).setUpClass()
+        client = api.Client(cls.cfg, api.json_handler)
+        cls.repos = tuple((
+            client.post(REPOSITORY_PATH, {'id': utils.uuid4()})
+            for _ in range(3)
         ))
-        cls.update_body = {'delta': {
-            key: uuid4() for key in ('description', 'display_name')
-        }}
-
-        # For convenience
-        read_url = urljoin(cls.cfg.base_url, cls.attrs_iter[0]['_href'])
-
-        # Read, update, and delete the three repositories, respectively.
         cls.responses = {}
-        cls.responses['read'] = requests.get(
-            read_url,
-            **cls.cfg.get_requests_kwargs()
-        )
-        cls.responses['read_importers'] = requests.get(
-            read_url + '?importers=true',
-            **cls.cfg.get_requests_kwargs()
-        )
-        cls.responses['read_distributors'] = requests.get(
-            read_url + '?distributors=true',
-            **cls.cfg.get_requests_kwargs()
-        )
-        cls.responses['read_details'] = requests.get(
-            read_url + '?details=true',
-            **cls.cfg.get_requests_kwargs()
-        )
-        cls.responses['update'] = requests.put(
-            urljoin(cls.cfg.base_url, cls.attrs_iter[1]['_href']),
-            json=cls.update_body,
-            **cls.cfg.get_requests_kwargs()
-        )
-        cls.responses['delete'] = requests.delete(
-            urljoin(cls.cfg.base_url, cls.attrs_iter[2]['_href']),
-            **cls.cfg.get_requests_kwargs()
-        )
+        client.response_handler = api.safe_handler
 
-    def test_status_codes(self):
+        # Read the first repo
+        path = cls.repos[0]['_href']
+        cls.responses['read'] = client.get(path)
+        for key in {'importers', 'distributors', 'details'}:
+            cls.responses['read_' + key] = client.get(path, params={key: True})
+
+        # Update the second repo
+        path = cls.repos[1]['_href']
+        cls.update_body = {'delta': {
+            key: utils.uuid4() for key in {'description', 'display_name'}
+        }}
+        cls.responses['update'] = client.put(path, cls.update_body)
+
+        # Delete the third.
+        cls.responses['delete'] = client.delete(cls.repos[2]['_href'])
+
+    def test_status_code(self):
         """Assert each response has a correct HTTP status code."""
         for key, response in self.responses.items():
             with self.subTest(key=key):
@@ -212,11 +200,10 @@ class ReadUpdateDeleteSuccessTestCase(TestCase):
 
     def test_read(self):
         """Assert the "read" response body contains the correct attributes."""
-        create_attrs = self.attrs_iter[0]
-        read_attrs = self.responses['read'].json()
-        self.assertLessEqual(set(create_attrs.keys()), set(read_attrs.keys()))
-        read_attrs = {key: read_attrs[key] for key in create_attrs.keys()}
-        self.assertEqual(create_attrs, read_attrs)
+        attrs = self.responses['read'].json()
+        self.assertLessEqual(set(self.repos[0].keys()), set(attrs.keys()))
+        attrs = {key: attrs[key] for key in self.repos[0].keys()}
+        self.assertEqual(self.repos[0], attrs)
 
     def test_read_imp_distrib(self):
         """Assert reading with importers/distributors returns correct attrs."""
@@ -248,10 +235,3 @@ class ReadUpdateDeleteSuccessTestCase(TestCase):
             with self.subTest(key=key):
                 self.assertIn(key, attrs['result'])
                 self.assertEqual(value, attrs['result'][key])
-
-    @classmethod
-    def tearDownClass(cls):
-        """Delete the created repositories."""
-        cls.attrs_iter = cls.attrs_iter[:-1]  # pop last item
-        for attrs in cls.attrs_iter:
-            delete(cls.cfg, attrs['_href'])
