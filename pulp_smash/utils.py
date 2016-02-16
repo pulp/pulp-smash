c31c35caf0259ac87d52b9a311c6d78fe021b52e
Jeremy Cline <jeremy@jcline.org>
2016-02-16 15:53:38 -0500

    Add tests for lazy on-demand syncing for RPM.
    Fix #73. This adds tests for consuming content from a repository that
    has been configured with the `on_demand` download policy. This also adds
    a method to reset Squid and its cache, and also fixes the `reset_pulp`
    method.
    
    Test results prior to this commit:
    
    Test results after this commit:

--- pulp_smash/utils.py
+++ pulp_smash/utils.py
@@ -69,8 +69,8 @@ def reset_pulp(server_config):
     prefix = '' if client.run(('id', '-u')).stdout.strip() == '0' else 'sudo '
     client.run('mongo pulp_database --eval db.dropDatabase()'.split())
     client.run('sudo -u apache pulp-manage-db'.split())
-    client.run((prefix + 'rm -rf /var/lib/pulp/content/*').split())
-    client.run((prefix + 'rm -rf /var/lib/pulp/published/*').split())
+    client.run((prefix + 'rm -rf /var/lib/pulp/content').split())
+    client.run((prefix + 'rm -rf /var/lib/pulp/published').split())
 
     for service in services:
         service.start()
@@ -106,3 +106,25 @@ class BaseAPITestCase(unittest2.TestCase):
         client = api.Client(cls.cfg)
         for resource in cls.resources:
             client.delete(resource)
+
+
+def reset_squid(server_config):
+    """Stop Squid, reset its cache directory, and restart it.
+
+    :param pulp_smash.config.ServerConfig server_config: Information about the
+        Pulp server being targeted.
+    :returns: Nothing.
+    """
+    squid_service = cli.Service(server_config, 'squid')
+    squid_service.stop()
+
+    # Clean out the cache directory and reinitialize it.
+    client = cli.Client(server_config)
+    prefix = '' if client.run(('id', '-u')).stdout.strip() == '0' else 'sudo '
+    client.run((prefix + 'rm -rf /var/spool/squid').split())
+    client.run((prefix + 'mkdir --context=system_u:object_r:squid_cache_t:s0' +
+                ' --mode=750 /var/spool/squid').split())
+    client.run((prefix + 'chown squid:squid /var/spool/squid').split())
+    client.run((prefix + 'squid -z').split())
+
+    squid_service.start()
