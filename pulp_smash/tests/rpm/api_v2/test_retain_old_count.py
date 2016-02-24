34631b1b751eeb4e6e1c75ae5b3a84b7997dcd06
Jeremy Audet <ichimonji10@gmail.com>
2016-02-24 10:39:09 -0500

    Refactor "retain_old_count" RPM tests
    Refactor module `pulp_smash.tests.rpm.api_v2.test_retain_old_count.py`:
    
    * Fiddle with docstrings and comments.
    * Fiddle with formatting.
    * Attempt to reduce the complexity of the variables handled by
      `RetainOldCountTestCase`. Most importantly, make the `responses` class
      attribute a flat dict by dropping the embedded lists.
    
    Test results do not change as a result of this commit.

--- pulp_smash/tests/rpm/api_v2/test_retain_old_count.py
+++ pulp_smash/tests/rpm/api_v2/test_retain_old_count.py
@@ -1,20 +1,19 @@
 # coding utf-8
 """Test the functionality in RPM repos when `retain_old_count`_ is specified.
 
-Following steps are executed in order to test correct functionality
-of repository created with valid feed and retain_old_count option set.
+Following steps are executed in order to test correct functionality of
+repository created with valid feed and `retain_old_count`_ option set.
 
-1. Create repository foo with valid feed, run sync, add distributor to it
-   and publish over http and https.
-2. Create second repository bar, with feed pointing to first repository,
-   set retain_old_count=0 and run sync.
+1. Create repository foo with valid feed, run sync, add distributor to it and
+   publish over http and https.
+2. Create second repository bar, with feed pointing to first repository, set
+   ``retain_old_count=0`` and run sync.
 3. Assert that repositories do not contain same set of units.
 4. Assert that number or RPMs in repo bar is less then in foo repo.
 
 .. _retain_old_count:
     https://pulp-rpm.readthedocs.org/en/latest/tech-reference/yum-plugins.html
 """
-
 from __future__ import unicode_literals
 
 try:  # try Python 3 import first
@@ -22,18 +21,14 @@ try:  # try Python 3 import first
 except ImportError:
     from urlparse import urljoin  # pylint:disable=C0411,E0401
 
-
 from pulp_smash import api, utils
-from pulp_smash.constants import (
-    REPOSITORY_PATH,
-    RPM_FEED_URL,
-)
+from pulp_smash.constants import REPOSITORY_PATH, RPM_FEED_URL
 
 _PUBLISH_DIR = 'pulp/repos/'
 
 
 def _gen_repo():
-    """Return a semi-random dict for use in creating an RPM repostirory."""
+    """Return a semi-random dict for use in creating an RPM repository."""
     return {
         'id': utils.uuid4(),
         'importer_config': {},
@@ -63,80 +58,77 @@ class RetainOldCountTestCase(utils.BaseAPITestCase):
     def setUpClass(cls):  # pylint:disable=arguments-differ
         """Create two repositories, first is feed of second one.
 
-        Provides server config and set of iterable to delete.
-        Following steps are executed:
-            1. Create repository foo with feed, sync and publish it.
-            2. Create repository bar with foo as a feed with retain-old-count=0
-            3. Run sync of repo foo.
-            3. Get information on both repositories.
+        Provides server config and set of iterable to delete. Following steps
+        are executed:
+
+        1. Create repository foo with feed, sync and publish it.
+        2. Create repository bar with foo as a feed with
+           ``retain_old_count=0``.
+        3. Run sync of repo foo.
+        4. Get information on both repositories.
         """
         super(RetainOldCountTestCase, cls).setUpClass()
-
+        client = api.Client(cls.cfg)
         cls.responses = {}
-        client = api.Client(cls.cfg, api.safe_handler)
+        hrefs = []  # repository hrefs
 
-        bodies = tuple((_gen_repo() for _ in range(2)))
-        # repo with feed from remote source
-        bodies[0]['importer_config']['feed'] = RPM_FEED_URL
-        repos = []
-        repos.append(client.post(REPOSITORY_PATH, bodies[0]).json())
-        sync_path = urljoin(repos[0]['_href'], 'actions/sync/')
-        # Run sync and wait for the task to complete
-        cls.responses['sync'] = []
-        cls.responses['sync'].append(client.post(
-            sync_path, {'override_config': {}}
-        ))
+        # Create and sync the first repository.
+        body = _gen_repo()
+        body['importer_config']['feed'] = RPM_FEED_URL
+        hrefs.append(client.post(REPOSITORY_PATH, body).json()['_href'])
+        cls.responses['first sync'] = client.post(
+            urljoin(hrefs[0], 'actions/sync/'),
+            {'override_config': {}}
+        )
+
         # Add distributor and publish
         cls.responses['distribute'] = client.post(
-            urljoin(repos[0]['_href'], 'distributors/'),
+            urljoin(hrefs[0], 'distributors/'),
             _gen_distributor(),
         )
-        cls.responses['publish'] = [client.post(
-            urljoin(repos[0]['_href'], 'actions/publish/'),
+        cls.responses['publish'] = client.post(
+            urljoin(hrefs[0], 'actions/publish/'),
             {'id': cls.responses['distribute'].json()['id']},
-        )]
-        # Use http feed instead of https to avoid possible config problems
-        # Repo with feed from 1st repo
-        bodies[1]['importer_config']['feed'] = urljoin(
-            # Create http url from base_url
+        )
+
+        # Create and sync the second repository. Ensure it fetches content from
+        # the first, and that the `retain_old_count` option is set correctly.
+        # We disable SSL validation for a practical reason: each HTTPS feed
+        # must have a certificate to work, which is burdensome to do here.
+        body = _gen_repo()
+        body['importer_config']['feed'] = urljoin(
             cls.cfg.base_url,
             _PUBLISH_DIR +
             cls.responses['distribute'].json()['config']['relative_url'],
         )
-        # set retain_old_count
-        bodies[1]['importer_config']['retain_old_count'] = 0
-        # Turning off validation is neccessary, as each https feed requires
-        # certificate to be specified
-        bodies[1]['importer_config']['ssl_validation'] = False
-        # Create and sync second repo
-        repos.append(client.post(REPOSITORY_PATH, bodies[1]).json())
-        sync_path = urljoin(repos[1]['_href'], 'actions/sync/')
-        cls.responses['sync'].append(client.post(
-            sync_path, {'override_config': {}}
-        ))
+        body['importer_config']['retain_old_count'] = 0  # see docstring
+        body['importer_config']['ssl_validation'] = False
+        hrefs.append(client.post(REPOSITORY_PATH, body).json()['_href'])
+        cls.responses['second sync'] = client.post(
+            urljoin(hrefs[1], 'actions/sync/'),
+            {'override_config': {}}
+        )
 
-        cls.repos = [client.get(repo['_href']).json() for repo in repos]
+        # Read the repositories and mark them for deletion.
+        cls.repos = [client.get(href).json() for href in hrefs]
+        cls.resources.update(set(hrefs))
 
     def test_status_code(self):
-        """Verify th HTTP status code of each server response."""
+        """Verify the HTTP status code of each server response."""
         for step, code in (
-                ('sync', 202),
-                ('publish', 202),
-        ):
-            with self.subTest(step=step):
-                for response in self.responses[step]:
-                    self.assertEqual(response.status_code, code)
-        for step, code in (
+                ('first sync', 202),
                 ('distribute', 201),
+                ('publish', 202),
+                ('second sync', 202),
         ):
             with self.subTest(step=step):
                 self.assertEqual(self.responses[step].status_code, code)
 
     def test_retain_old_count_works(self):
-        """Test that content_units_counts in repositories differ.
+        """Test that ``content_units_counts`` in repositories differ.
 
-        Test that second repo has less rpm units than first repo due to
-        retain_old_count option value set.
+        Test that second repo has less rpm units than first repo due to the
+        ``retain_old_count`` option being set.
         """
         counts = [repo.get('content_unit_counts', {}) for repo in self.repos]
         self.assertNotEqual(counts[0], counts[1])
