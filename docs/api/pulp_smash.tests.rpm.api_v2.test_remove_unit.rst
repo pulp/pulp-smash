524aa5360cd94199deb80ebc0d0be69d9943d8d2
Jeremy Audet <ichimonji10@gmail.com>
2016-02-26 10:36:12 -0500

    Refactor RPM API tests for removing units
    Refactor module `pulp_smash.tests.rpm.api_v2.test_remove_unit`:
    
    * Change the encoding declaration at the top of the file. Do this
      because the existing encoding declaration is invalid. From PEP 263
      [1]: "the first or second line must match the regular expression
      "coding[:=]\s*([-\w.]+)"
    * Perform significant reformatting. As a side effect, remove many
      `pylint:disable` directives from the module.
    * Encapsulate some common tasks as private functions. At least one of
      these, `_sync_repo`, can be moved into module
      `pulp_smash.tests.rpm.api_v2.utils` and re-used.
    * Use function from module `pulp_smash.tests.rpm.api_v2.utils`.
    * Fix broken and incorrect docstring segments.
    * Flatten the `cls.responses` variable, in an attempt to make it easier
      to think about.
    
    In addition, tweak the documentation stub for this module.
    
    These changes do not affect test suite results.
    
    [1] https://www.python.org/dev/peps/pep-0263/

--- docs/api/pulp_smash.tests.rpm.api_v2.test_remove_unit.rst
+++ docs/api/pulp_smash.tests.rpm.api_v2.test_remove_unit.rst
@@ -1,5 +1,5 @@
 `pulp_smash.tests.rpm.api_v2.test_remove_unit`
-===============================================
+==============================================
 
 Location: :doc:`/index` → :doc:`/api` →
 :doc:`/api/pulp_smash.tests.rpm.api_v2.test_remove_unit`
--- pulp_smash/tests/rpm/api_v2/test_remove_unit.py
+++ pulp_smash/tests/rpm/api_v2/test_remove_unit.py
@@ -1,62 +1,51 @@
-# coding utf-8
+# coding=utf-8
 """Test the functionality in RPM repos when `remove_missing`_ is set to True.
 
 Following steps are executed in order to test correct functionality
 of repository created with valid feed and remove_missing option set.
 
-1. Create repository foo with valid feed, run sync, add distributor to it
-   and publish over http and https.
-2. Create second repository bar, with feed pointing to first repository,
-   set remove_missing=True and run sync on them.
+1. Create repository foo with valid feed, run sync, add distributor to it and
+   publish over http and https.
+2. Create second repository bar, with feed pointing to first repository, set
+   ``remove_missing=True`` and run sync on them.
 3. Assert that repositories contain same set of units.
 4. Remove random unit from repository foo and publish.
 5. Sync bar repository.
 6. Assert that both repositories contain same units.
 
-.. _remove missing:
+.. _remove_missing:
     https://pulp-rpm.readthedocs.org/en/latest/tech-reference/yum-plugins.html
 """
 
 from __future__ import unicode_literals
 
 import random
-
 try:  # try Python 3 import first
     from urllib.parse import urljoin
 except ImportError:
     from urlparse import urljoin  # pylint:disable=C0411,E0401
 
-
 from pulp_smash import api, utils
-from pulp_smash.constants import (
-    REPOSITORY_PATH,
-    RPM_FEED_URL,
-)
+from pulp_smash.constants import REPOSITORY_PATH, RPM_FEED_URL
+from pulp_smash.tests.rpm.api_v2.utils import gen_distributor, gen_repo
 
 _PUBLISH_DIR = 'pulp/repos/'
 
 
-def _gen_repo():
-    """Return a semi-random dict for use in creating an RPM repostirory."""
-    return {
-        'id': utils.uuid4(),
-        'importer_config': {},
-        'importer_type_id': 'yum_importer',
-        'notes': {'_repo-type': 'rpm-repo'},
-    }
+# This can be moved to `pulp_smash.tests.rpm.api_v2.utils` and re-used.
+def _sync_repo(server_config, repo_href):
+    """Sync the referenced repository. Return the raw server response."""
+    return api.Client(server_config).post(
+        urljoin(repo_href, 'actions/sync/'),
+        {'override_config': {}}
+    )
 
 
-def _gen_distributor():
-    """Return a semi-random dict for use in creating a YUM distributor."""
+def _get_rpm_ids(search_body):
+    """Get RPM unit IDs from search results. Return a set."""
     return {
-        'auto_publish': False,
-        'distributor_id': utils.uuid4(),
-        'distributor_type_id': 'yum_distributor',
-        'distributor_config': {
-            'http': True,
-            'https': True,
-            'relative_url': utils.uuid4() + '/',
-        },
+        unit['unit_id'] for unit in search_body
+        if unit['unit_type_id'] == 'rpm'
     }
 
 
@@ -64,111 +53,120 @@ class RemoveMissingTestCase(utils.BaseAPITestCase):
     """Test functionality of --remove-missing option enabled."""
 
     @classmethod
-    def setUpClass(cls):  # pylint:disable=arguments-differ
+    def setUpClass(cls):
         """Create two repositories, first is feed of second one.
 
-        Provides server config and set of iterable to delete.
-        Following steps are executed:
-            1. Create repository foo with feed, sync and publish it.
-            2. Create repository bar with foo as a feed and run sync.
-            3. Get content of both repositories.
-            4. Remove random unit from repository foo and publish foo.
-            5. Sync repository bar.
-            6. Get content of both repositories.
+        Provides server config and set of iterable to delete. Following steps
+        are executed:
+
+        1. Create repository foo with feed, sync and publish it.
+        2. Create repository bar with foo as a feed and run sync.
+        3. Get content of both repositories.
+        4. Remove random unit from repository foo and publish foo.
+        5. Sync repository bar.
+        6. Get content of both repositories.
         """
         super(RemoveMissingTestCase, cls).setUpClass()
-
         cls.responses = {}
-        client = api.Client(cls.cfg, api.safe_handler)
+        hrefs = []  # repository hrefs
 
-        bodies = tuple((_gen_repo() for _ in range(2)))
-        bodies[0]['importer_config']['feed'] = RPM_FEED_URL
-        repos = []
-        repos.append(client.post(REPOSITORY_PATH, bodies[0]).json())
-        sync_path = urljoin(repos[0]['_href'], 'actions/sync/')
-        # Run sync and wait for the task to complete
-        cls.responses['sync'] = []
-        cls.responses['sync'].append(client.post(
-            sync_path, {'override_config': {}}
-        ))
-        # Add distributor and publish
+        # Create and sync a repository.
+        client = api.Client(cls.cfg)
+        body = gen_repo()
+        body['importer_config']['feed'] = RPM_FEED_URL
+        hrefs.append(client.post(REPOSITORY_PATH, body).json()['_href'])
+        cls.resources.add(hrefs[0])  # mark for deletion
+        cls.responses['first sync'] = _sync_repo(cls.cfg, hrefs[0])
+
+        # Add a distributor and publish it.
         cls.responses['distribute'] = client.post(
-            urljoin(repos[0]['_href'], 'distributors/'),
-            _gen_distributor(),
+            urljoin(hrefs[0], 'distributors/'),
+            gen_distributor(),
         )
-        cls.responses['publish'] = []
-        cls.responses['publish'].append(client.post(
-            urljoin(repos[0]['_href'], 'actions/publish/'),
+        cls.responses['first publish'] = client.post(
+            urljoin(hrefs[0], 'actions/publish/'),
             {'id': cls.responses['distribute'].json()['id']},
-        ))
+        )
 
-        # Use http feed instead of https to avoid possible config problems
-        bodies[1]['importer_config']['feed'] = urljoin(
-            # Create http url from base_url
+        # Create and sync a second repository. We disable SSL validation for a
+        # practical reason: each HTTPS feed must have a certificate to work,
+        # which is burdensome to do here.
+        body = gen_repo()
+        body['importer_config']['feed'] = urljoin(
             cls.cfg.base_url,
             _PUBLISH_DIR +
             cls.responses['distribute'].json()['config']['relative_url'],
         )
-        bodies[1]['importer_config']['remove_missing'] = True
-        # Turning off validation is neccessary, as each https feed requires
-        # certificate to be specified
-        bodies[1]['importer_config']['ssl_validation'] = False
-        # Create and sync second repo
-        repos.append(client.post(REPOSITORY_PATH, bodies[1]).json())
-        sync_path = urljoin(repos[1]['_href'], 'actions/sync/')
-        cls.responses['sync'].append(client.post(
-            sync_path, {'override_config': {}}
-        ))
-        # Get content of both repositories
-        body = {'criteria': {}}
-        cls.responses['units before removal'] = [
-            client.post(urljoin(repo['_href'], 'search/units/'), body)
-            for repo in repos
-        ]
+        body['importer_config']['remove_missing'] = True  # see docstring
+        body['importer_config']['ssl_validation'] = False
+        hrefs.append(client.post(REPOSITORY_PATH, body).json()['_href'])
+        cls.resources.add(hrefs[1])  # mark for deletion
+        cls.responses['second sync'] = _sync_repo(cls.cfg, hrefs[1])
+
+        # Get contents of both repositories
+        for i, href in enumerate(hrefs):
+            cls.responses['repo {} units, pre'.format(i)] = client.post(
+                urljoin(href, 'search/units/'),
+                {'criteria': {}},
+            )
+
         # Get random unit from first repository to remove
-        rpms = [unit['metadata']['name']
-                for unit in cls.responses['units before removal'][0].json()
-                if unit['unit_type_id'] == 'rpm']
-        cls.removed_unit = random.choice(rpms)
+        cls.removed_unit = random.choice([
+            unit['metadata']['name']
+            for unit in cls.responses['repo 0 units, pre'].json()
+            if unit['unit_type_id'] == 'rpm'
+        ])
+
         # Remove unit from first repo and publish again
         cls.responses['remove unit'] = client.post(
-            urljoin(repos[0]['_href'], 'actions/unassociate/'),
-            {'criteria':
-                {'fields':  # pylint:disable=bad-continuation
-                    {'unit': ['name', 'epoch', 'version', 'release',  # noqa pylint:disable=bad-continuation,line-too-long
-                              'arch', 'checksum', 'checksumtype']},
-                    'type_ids': ['rpm'],  # noqa pylint:disable=bad-continuation,line-too-long
-                    'filters': {'unit': {'name': cls.removed_unit}}}},)  # noqa pylint:disable=bad-continuation,line-too-long
-        # Publish first repo again
-        cls.responses['publish'].append(client.post(
-            urljoin(repos[0]['_href'], 'actions/publish/'),
+            urljoin(hrefs[0], 'actions/unassociate/'),
+            {
+                'criteria': {
+                    'fields': {
+                        'unit': [
+                            'arch',
+                            'checksum',
+                            'checksumtype',
+                            'epoch',
+                            'name',
+                            'release',
+                            'version',
+                        ]
+                    },
+                    'type_ids': ['rpm'],
+                    'filters': {
+                        'unit': {'name': cls.removed_unit}
+                    }
+                }
+            },
+        )
+
+        # Publish the first repo again, and sync the second repo again.
+        cls.responses['second publish'] = client.post(
+            urljoin(hrefs[0], 'actions/publish/'),
             {'id': cls.responses['distribute'].json()['id']},
-        ))
-        # Sync second repo
-        sync_path = urljoin(repos[1]['_href'], 'actions/sync/')
-        cls.responses['sync'].append(client.post(
-            sync_path, {'override_config': {}}
-        ))
+        )
+        cls.responses['third sync'] = _sync_repo(cls.cfg, hrefs[1])
+
         # Search for units in both repositories again
-        cls.responses['units after removal'] = [
-            client.post(urljoin(repo['_href'], 'search/units/'), body)
-            for repo in repos
-        ]
-        for repo in repos:
-            cls.resources.add(repo['_href'])
+        for i, href in enumerate(hrefs):
+            cls.responses['repo {} units, post'.format(i)] = client.post(
+                urljoin(href, 'search/units/'),
+                {'criteria': {}},
+            )
 
     def test_status_code(self):
-        """Verify th HTTP status code of each server response."""
+        """Verify the HTTP status code of each server response."""
         for step, code in (
-                ('sync', 202),
-                ('publish', 202),
-                ('units before removal', 200),
-                ('units after removal', 200),
-        ):
-            with self.subTest(step=step):
-                for response in self.responses[step]:
-                    self.assertEqual(response.status_code, code)
-        for step, code in (
+                ('first sync', 202),
+                ('second sync', 202),
+                ('third sync', 202),
+                ('first publish', 202),
+                ('second publish', 202),
+                ('repo 0 units, pre', 200),
+                ('repo 1 units, pre', 200),
+                ('repo 0 units, post', 200),
+                ('repo 1 units, post', 200),
                 ('distribute', 201),
                 ('remove unit', 202),
         ):
@@ -176,30 +174,30 @@ class RemoveMissingTestCase(utils.BaseAPITestCase):
                 self.assertEqual(self.responses[step].status_code, code)
 
     def test_units_before_removal(self):
-        """Test that units in repositories before removal are the same."""
-        bodies = [re.json() for re in self.responses['units before removal']]
-        # Package category and package group will differ so we count only RPMs
+        """Assert the repositories have the same units before the removal.
+
+        Package category and package group differ, so we count only RPM units.
+        """
         self.assertEqual(
-            set(unit['unit_id'] for unit in bodies[0]
-                if unit['unit_type_id'] == 'rpm'),  # This test is fragile
-            set(unit['unit_id'] for unit in bodies[1]
-                if unit['unit_type_id'] == 'rpm'),  # due to hard-coded
-        )  # indices. But the data is complex, and this makes things simpler.
+            _get_rpm_ids(self.responses['repo 0 units, pre'].json()),
+            _get_rpm_ids(self.responses['repo 1 units, pre'].json()),
+        )
+
+    def test_units_after_removal(self):
+        """Assert the repositories have the same units after the removal.
+
+        Package category and package group differ, so we count only RPM units.
+        """
+        self.assertEqual(
+            _get_rpm_ids(self.responses['repo 0 units, post'].json()),
+            _get_rpm_ids(self.responses['repo 1 units, post'].json()),
+        )
 
     def test_unit_removed(self):
         """Test that correct unit from first repository has been removed."""
-        body = self.responses['units after removal'][0].json()
-        units_names = set(unit['metadata']['name'] for unit in body
-                          if unit['unit_type_id'] == 'rpm')
+        body = self.responses['repo 0 units, post'].json()
+        units_names = set(
+            unit['metadata']['name'] for unit in body
+            if unit['unit_type_id'] == 'rpm'
+        )
         self.assertNotIn(self.removed_unit, units_names)
-
-    def test_units_after_removal(self):
-        """Test that units in repositories after removal are the same."""
-        bodies = [re.json() for re in self.responses['units after removal']]
-        # Package category and package group will differ so we count only RPMs
-        self.assertEqual(
-            set(unit['unit_id'] for unit in bodies[0]
-                if unit['unit_type_id'] == 'rpm'),  # This test is fragile
-            set(unit['unit_id'] for unit in bodies[1]
-                if unit['unit_type_id'] == 'rpm'),  # due to hard-coded
-        )  # indices. But the data is complex, and this makes things simpler.
