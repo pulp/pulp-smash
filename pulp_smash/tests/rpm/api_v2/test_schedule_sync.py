00989f19d3657d92fa30147c188a226f3087b0a7
Jeremy Audet <ichimonji10@gmail.com>
2016-03-04 12:47:25 -0500

    Edit RPM repository sync scheduling tests
    Edit the newly added module
    `pulp_smash.tests.rpm.api_v2.test_schedule_sync`:
    
    * Make all code compatible with Python 2 and 3. Formerly, the code was
      only compatible with Python 2.
    * Conditionally skip an assertion in
      `CreateFailureTestCase.test_status_code`. One of the assertions fails
      against Pulp 2.7 due to Pulp issue 1745. [1]
    * Conditionally skip `ScheduledSyncTestCase.test_total_run_count` due to
      https://pulp.plan.io/issues/1748.
    * More heavily isolate various assertions, so that a single assertion
      failure does not affect other assertions.
    * Pull some common code out into a mixin, `CreateRepoMixin`. Pull some
      common data out into private constants.
    * Add method `ReadUpdateDeleteTestCase.test_delete`.
    * As usual, reformat code and fiddle with docstrings.
    
    Test results are typically OK:
    
        =========  ==========  ==============
        Pulp Ver.  Num. Tests  Test Results
        =========  ==========  ==============
        2.7        11          OK (skipped=1)
        dev (2.8)  11          OK (skipped=1)
        =========  ==========  ==============
    
    Test results generated with:
    
        python -m unittest2 pulp_smash.tests.rpm.api_v2.test_schedule_sync
    
    [1] https://pulp.plan.io/issues/1745

--- pulp_smash/tests/rpm/api_v2/test_schedule_sync.py
+++ pulp_smash/tests/rpm/api_v2/test_schedule_sync.py
@@ -7,7 +7,6 @@ This module assumes that the tests in
 .. _syncronization:
     https://pulp.readthedocs.org/en/latest/dev-guide/integration/rest-api/repo/sync.html#scheduling-a-sync
 """
-
 from __future__ import unicode_literals
 
 import time
@@ -16,12 +15,50 @@ try:  # try Python 3 import first
 except ImportError:
     from urlparse import urljoin  # pylint:disable=C0411,E0401
 
-from pulp_smash import api, utils
+from packaging.version import Version
+
+from pulp_smash import api, selectors, utils
 from pulp_smash.constants import REPOSITORY_PATH, RPM_FEED_URL
 from pulp_smash.tests.rpm.api_v2.utils import gen_repo
 
+_MUTABLE_ATTRS = {
+    'consecutive_failures',
+    'last_run_at',
+    'last_updated',
+    'next_run',
+    'first_run',
+    'remaining_runs',
+    'total_run_count',
+}
+# Pulp may, of its own accord, change these schedule attributes.
 
-class CreateSuccessTestCase(utils.BaseAPITestCase):
+_SCHEDULE = {'schedule': 'PT30S'}
+# Sync every 30 seconds.
+
+_SCHEDULE_PATH = 'importers/{}/schedules/sync/'
+# Usage: urljoin(repo['_href'], _SCHEDULE_PATH.format(importer_type_id))
+
+
+# It's OK that this class has one method. It's an intentionally small class.
+class CreateRepoMixin(object):  # pylint:disable=too-few-public-methods
+    """Provide a method for creating a repository."""
+
+    @classmethod
+    def create_repo(cls):
+        """Create a semi-random RPM repository with a valid RPM feed URL.
+
+        Add this repository's href to ``cls.resources``. Return a two-tuple of
+        ``(href, importer_type_id)``. This method requires a server config,
+        ``cls.cfg``, and a set, ``cls.resources``.
+        """
+        body = gen_repo()
+        body['importer_config']['feed'] = RPM_FEED_URL
+        href = api.Client(cls.cfg).post(REPOSITORY_PATH, body).json()['_href']
+        cls.resources.add(href)
+        return (href, body['importer_type_id'])
+
+
+class CreateSuccessTestCase(CreateRepoMixin, utils.BaseAPITestCase):
     """Establish that we can create a schedule to sync the repository."""
 
     @classmethod
@@ -29,39 +66,32 @@ class CreateSuccessTestCase(utils.BaseAPITestCase):
         """Create an RPM repo with a valid feed, create a schedule to sync it.
 
         Do the following:
+
         1. Create a repository with a valid feed
         2. Schedule sync to run every 30 seconds
         """
         super(CreateSuccessTestCase, cls).setUpClass()
-        client = api.Client(cls.cfg)
+        href, importer_type_id = cls.create_repo()
 
-        # Create a repo with a valid feed
-        body = gen_repo()
-        body['importer_config']['feed'] = RPM_FEED_URL
-        repo = client.post(REPOSITORY_PATH, body).json()
-        cls.resources.add(repo['_href'])
-
-        # Schedule a sync to run every 30 seconds
-        scheduling_url = '/'.join([
-            'importers', body['importer_type_id'], 'schedules/sync/'
-        ])
-        cls.response = client.post(
-            urljoin(repo['_href'], scheduling_url),
-            {'schedule': 'PT30S'}
-        )
+        # Schedule a sync
+        path = urljoin(href, _SCHEDULE_PATH.format(importer_type_id))
+        cls.response = api.Client(cls.cfg).post(path, _SCHEDULE)
+        cls.response_json = cls.response.json()
 
     def test_status_code(self):
         """Assert the response has an HTTP 201 status code."""
         self.assertEqual(self.response.status_code, 201)
 
-    def test_count_enabled(self):
-        """Validate the ``total_run_count`` and ``enabled`` attributes."""
-        attrs = self.response.json()
-        self.assertEqual(attrs.get('total_run_count'), 0)
-        self.assertTrue(attrs.get('enabled'))
+    def test_total_run_count(self):
+        """Verify the ``total_run_count`` attribute in the response."""
+        self.assertEqual(self.response_json['total_run_count'], 0)
+
+    def test_enabled(self):
+        """Verify the ``enabled`` attribute in the response."""
+        self.assertTrue(self.response_json['enabled'])
 
 
-class CreateFailureTestCase(utils.BaseAPITestCase):
+class CreateFailureTestCase(CreateRepoMixin, utils.BaseAPITestCase):
     """Establish that schedules are not created in `documented scenarios`_.
 
     .. _documented scenarios:
@@ -70,58 +100,57 @@ class CreateFailureTestCase(utils.BaseAPITestCase):
 
     @classmethod
     def setUpClass(cls):
-        """Create several schedules.
+        """Intentionally fail at creating several sync schedules for a repo.
 
-        Each schedule is created to test a different failure scenario.
+        Each schedule tests a different failure scenario.
         """
         super(CreateFailureTestCase, cls).setUpClass()
+        href, importer_type_id = cls.create_repo()
+
+        # We'll need these below.
+        scheduling_path = _SCHEDULE_PATH.format(importer_type_id)
+        scheduling_path_bad = _SCHEDULE_PATH.format(utils.uuid4())
+        bad_repo_path = '{}/{}/'.format(REPOSITORY_PATH, utils.uuid4())
+
+        # Use client to get paths with bodies. Save responses and status_codes.
         client = api.Client(cls.cfg)
-
-        # Create a repo with a valid feed
-        body = gen_repo()
-        body['importer_config']['feed'] = RPM_FEED_URL
-        repo = client.post(REPOSITORY_PATH, body).json()
-        cls.resources.add(repo['_href'])
-
         client.response_handler = api.echo_handler
+        paths = (
+            urljoin(href, scheduling_path),
+            urljoin(href, scheduling_path),
+            urljoin(href, scheduling_path),
+            urljoin(href, scheduling_path),
+            urljoin(href, scheduling_path_bad),
+            urljoin(bad_repo_path, scheduling_path)
+        )
         cls.bodies = (
             {'schedule': None},  # 400
-            {'unknown': 'parameter', 'schedule': 'PT30S'},  # 400
+            {'schedule': 'PT30S', 'unknown': 'parameter'},  # 400
             ['Incorrect data type'],  # 400
             {'missing_required_keys': 'schedule'},  # 400
-            {'schedule': 'PT30S'},  # tests incorrect importer in url, 404
-            {'schedule': 'PT30S'},  # tests incorrect repo in url, 404
+            _SCHEDULE,  # tests incorrect importer in url, 404
+            _SCHEDULE,  # tests incorrect repo in url, 404
         )
-        scheduling_url = '/'.join([
-            'importers', body['importer_type_id'], 'schedules/sync/'
-        ])
-        bad_importer_url = '/'.join([
-            'importers', utils.uuid4(), 'schedules/sync/'
-        ])
-        bad_repo_path = '/'.join([REPOSITORY_PATH, utils.uuid4()])
-        cls.paths = (
-            urljoin(repo['_href'], scheduling_url),
-            urljoin(repo['_href'], scheduling_url),
-            urljoin(repo['_href'], scheduling_url),
-            urljoin(repo['_href'], scheduling_url),
-            urljoin(repo['_href'], bad_importer_url),
-            urljoin(bad_repo_path, scheduling_url)
-        )
+        cls.responses = tuple((
+            client.post(path, body) for path, body in zip(paths, cls.bodies)
+        ))
         cls.status_codes = (400, 400, 400, 400, 404, 404)
-        cls.responses = [
-            client.post(path, req_body) for path, req_body in zip(
-                cls.paths, cls.bodies)
-        ]
 
     def test_status_code(self):
         """Assert that each response has the expected HTTP status code."""
         for body, response, status_code in zip(
-                self.bodies, self.responses, self.status_codes):
+                self.bodies,
+                self.responses,
+                self.status_codes):
+            if (body == ['Incorrect data type'] and
+                    self.cfg.version < Version('2.8') and
+                    selectors.bug_is_untestable(1745)):
+                continue
             with self.subTest(body=body):
                 self.assertEqual(response.status_code, status_code)
 
 
-class ReadUpdateDeleteTestCase(utils.BaseAPITestCase):
+class ReadUpdateDeleteTestCase(CreateRepoMixin, utils.BaseAPITestCase):
     """Establish that we can `read`_, `update`_ and `delete`_ schedules.
 
     This test case assumes the assertions in :class:`CreateSuccessTestCase`
@@ -139,45 +168,26 @@ class ReadUpdateDeleteTestCase(utils.BaseAPITestCase):
     def setUpClass(cls):
         """Create three schedules and read, update and delete them."""
         super(ReadUpdateDeleteTestCase, cls).setUpClass()
-        client = api.Client(cls.cfg, api.json_handler)
-
-        # Create a repo with a valid feed
-        body = gen_repo()
-        body['importer_config']['feed'] = RPM_FEED_URL
-        repo = client.post(REPOSITORY_PATH, body)
-        cls.resources.add(repo['_href'])
+        href, importer_type_id = cls.create_repo()
+        cls.schedules = []
+        cls.responses = {}
+        cls.update_body = {'schedule': 'PT1M'}
 
         # Create schedules
-        scheduling_url = '/'.join([
-            'importers', body['importer_type_id'], 'schedules/sync/'
-        ])
-        scheduling_path = urljoin(repo['_href'], scheduling_url)
-        cls.schedules = tuple((
-            client.post(scheduling_path, {'schedule': 'PT30S'})
-            for _ in range(3)
-        ))
-        cls.responses = {}
+        client = api.Client(cls.cfg, api.json_handler)
+        path = urljoin(href, _SCHEDULE_PATH.format(importer_type_id))
+        for _ in range(3):
+            cls.schedules.append(client.post(path, _SCHEDULE))
+
+        # Read the first schedule and all schedules, update the second
+        # schedule, and delete the third schedule.
         client.response_handler = api.safe_handler
-
-        # Attributes that may be changed after creation
-        cls.mutable_attrs = [
-            'consecutive_failures', 'last_run_at', 'last_updated', 'next_run',
-            'first_run', 'remaining_runs', 'total_run_count'
-        ]
-
-        # Read the first schedule
         cls.responses['read_one'] = client.get(cls.schedules[0]['_href'])
-
-        # Read all schedules for the repo
-        cls.responses['read_many'] = client.get(scheduling_path)
-
-        # Update the second schedule
-        cls.update_body = {'schedule': 'PT1M'}
+        cls.responses['read_many'] = client.get(path)
         cls.responses['update'] = client.put(
-            cls.schedules[1]['_href'], cls.update_body
+            cls.schedules[1]['_href'],
+            cls.update_body,
         )
-
-        # Delete the third schedule
         cls.responses['delete'] = client.delete(cls.schedules[2]['_href'])
 
     def test_status_code(self):
@@ -189,36 +199,41 @@ class ReadUpdateDeleteTestCase(utils.BaseAPITestCase):
     def test_read_one(self):
         """Assert the "read_one" response contains the correct attributes."""
         attrs = self.responses['read_one'].json()
-        self.assertEqual(set(self.schedules[0]), set(attrs))
-        attrs = {key: attrs[key] for key in attrs
-                 if key not in self.mutable_attrs}
-        for key in attrs:
+        self.assertEqual(set(self.schedules[0]), set(attrs))  # keys
+        for key, value in attrs.items():
+            if key in _MUTABLE_ATTRS:
+                continue
             with self.subTest(key=key):
-                self.assertEqual(self.schedules[0][key], attrs[key])
+                self.assertEqual(value, self.schedules[0][key])
 
     def test_read_many(self):
-        """Assert the "read_many" response body contains all schedules."""
+        """Assert the "read_many" response body contains all schedule hrefs."""
         attrs = self.responses['read_many'].json()
-        self.assertEqual(len(attrs), 3)
         expected_hrefs = {schedule['_href'] for schedule in self.schedules}
         read_hrefs = {schedule['_href'] for schedule in attrs}
         self.assertEqual(expected_hrefs, read_hrefs)
+        # Ensure set deduplication didn't do anything funny.
+        self.assertEqual(len(self.schedules), len(attrs))
 
     def test_update(self):
         """Assert the "update" response body has the correct attributes."""
+        expect = self.schedules[1].copy()
+        expect.update(self.update_body)
         attrs = self.responses['update'].json()
-        self.assertEqual(set(self.schedules[1]), set(attrs))
-        for key in self.update_body:
+        self.assertEqual(set(expect), set(attrs))  # keys
+
+        for key, value in expect.items():
+            if key in _MUTABLE_ATTRS:
+                continue
             with self.subTest(key=key):
-                self.assertEqual(self.update_body[key], attrs[key])
-        attrs = {key: attrs[key] for key in attrs
-                 if key not in self.mutable_attrs + self.update_body.keys()}
-        for key in attrs:
-            with self.subTest(key=key):
-                self.assertEqual(self.schedules[1][key], attrs[key])
+                self.assertEqual(value, attrs[key])
+
+    def test_delete(self):
+        """Assert the "delete" response body is null."""
+        self.assertIsNone(self.responses['delete'].json())
 
 
-class ScheduledSyncTestCase(utils.BaseAPITestCase):
+class ScheduledSyncTestCase(CreateRepoMixin, utils.BaseAPITestCase):
     """Establish that sync runs according to the specified schedule.
 
     This test case assumes the assertions in :class:`CreateSuccessTestCase`
@@ -230,34 +245,29 @@ class ScheduledSyncTestCase(utils.BaseAPITestCase):
         """Create a schedule to sync the repo, verify the ``total_run_count``.
 
         Do the following:
+
         1. Create a repository with a valid feed
         2. Schedule sync to run every 30 seconds
         3. Wait for 40 seconds and read the schedule to get the number of
-        "sync" runs.
+           "sync" runs.
+
         """
         super(ScheduledSyncTestCase, cls).setUpClass()
+        href, importer_type_id = cls.create_repo()
+
+        # Schedule a sync to run every 30 seconds. Wait 40 seconds and read it.
         client = api.Client(cls.cfg, api.json_handler)
-
-        # Create a repo with a valid feed
-        body = gen_repo()
-        body['importer_config']['feed'] = RPM_FEED_URL
-        repo = client.post(REPOSITORY_PATH, body)
-        cls.resources.add(repo['_href'])
-
-        # Schedule a sync to run every 30 seconds
-        scheduling_url = '/'.join([
-            'importers', body['importer_type_id'], 'schedules/sync/'
-        ])
-        schedule_path = urljoin(repo['_href'], scheduling_url)
-        schedule = client.post(schedule_path, {'schedule': 'PT30S'})
-
-        # Wait for sync to run
+        schedule_path = urljoin(href, _SCHEDULE_PATH.format(importer_type_id))
+        schedule = client.post(schedule_path, _SCHEDULE)
         time.sleep(40)
-
-        # Read the schedule
         cls.response = client.get(schedule['_href'])
 
-    def test_scheduled_sync(self):
-        """Assert the sync ran successfully twice in the past 40 seconds."""
+    def test_total_run_count(self):
+        """Assert the sync ran twice in the past 40 seconds."""
+        if selectors.bug_is_untestable(1748):
+            self.skipTest('https://pulp.plan.io/issues/1748')
         self.assertEqual(self.response['total_run_count'], 2)
+
+    def test_consecutive_failures(self):
+        """Assert the sync encountered no consecutive failures."""
         self.assertEqual(self.response['consecutive_failures'], 0)
