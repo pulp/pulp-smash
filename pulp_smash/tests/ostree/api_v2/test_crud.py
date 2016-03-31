1316c17f0d7a2d0ba134f287cdc3f2d842901e04
Jeremy Audet <ichimonji10@gmail.com>
2016-03-31 15:14:51 -0400

    Update new OSTree distributor tests
    Make at least the following changes:
    
    * Rename class `CreateDistributors` to `CreateDistributorsTestCase`. Do
      likewise for class `UpdateDistributors`.
    * Rewrite the docstrings on both classes.
    * Expand the kinds of inputs fed to Pulp, and randomize the inputs
      instead of using hard-coded values. Update assertions accordingly.
    * Make class `UpdateDistributorsTestCase` create two repositories, not
      three. There was no apparent use for the third repository.
    * Ensure the assertions in each test case are unique. For example, only
      `CreateDistributorsTestCase` now makes assertions about the status
      code returned by Pulp when creating a distributor, instead of both
      test cases.
    * Rewrite the exact implementation of both test cases. For example,
      given a collection of homogeneous responses, place them in a list
      instead of creating one variable for each.
    * Move test skipping logic from `setUpClass` methods into test methods.
      Though these test cases trigger https://pulp.plan.io/issues/1106, much
      of their logic is valid on existing Pulp systems. Skipping only the
      known-failing assertions lets us exercise those working parts of Pulp.
      It also gives greater assurance that these new test cases will work
      correctly when the Pulp issue is fixed.
    
    All relevant tests still pass, though the number of assertions changes.
    Test results generated with:
    
        python -m unittest2 pulp-smash.tests.ostree.api_v2.test_crud

--- pulp_smash/tests/ostree/api_v2/test_crud.py
+++ pulp_smash/tests/ostree/api_v2/test_crud.py
@@ -17,7 +17,6 @@ following trees of assumptions are explored in this module::
    http://pulp.readthedocs.org/en/latest/dev-guide/integration/rest-api/repo/cud.html
 """
 from __future__ import unicode_literals
-import copy
 
 from pulp_smash import api, selectors, utils
 from pulp_smash.compat import urljoin
@@ -28,6 +27,20 @@ from pulp_smash.tests.ostree.utils import gen_repo, skip_if_no_plugin
 def setUpModule():  # pylint:disable=invalid-name
     """Skip tests if the OSTree plugin is not installed."""
     skip_if_no_plugin()
+
+
+def _gen_distributor(relative_path):
+    """Return a dict for use in creating a distributor."""
+    return {
+        'config': {},
+        'distributor_config': {'relative_path': relative_path},
+        'distributor_type_id': 'ostree_web_distributor',
+    }
+
+
+def _gen_rel_path(segments=2):
+    """Return a semi-random relative path."""
+    return '/'.join((utils.uuid4() for _ in range(segments)))
 
 
 class CreateTestCase(utils.BaseAPITestCase):
@@ -79,124 +92,125 @@ class CreateTestCase(utils.BaseAPITestCase):
                 self.assertEqual(importers[0][key], body['importer_' + key])
 
 
-class CreateDistributors(utils.BaseAPITestCase):
-    """Test the creation of ostree distributors."""
+class CreateDistributorsTestCase(utils.BaseAPITestCase):
+    """Show Pulp can create OSTree distributors and prevent path conflicts.
+
+    It is valid for the following distributor relative paths to coexist:
+
+    * ``foo/bar``
+    * ``foo/biz``
+    * ``foo/baz/abc``
+
+    But given the above, the following distributor relative paths conflict:
+
+    * ``foo/bar``
+    * ``foo/bar/biz``
+    * ``/foo/bar``
+    """
 
     @classmethod
     def setUpClass(cls):
-        """Create distributors that have conflicting relative_paths."""
-        if selectors.bug_is_untestable(1106):
-            cls.skipTest(cls, 'https://pulp.plan.io/issues/1106')
-        super(CreateDistributors, cls).setUpClass()
-        client = api.Client(cls.cfg, api.echo_handler)
-        cls.bodies = tuple((gen_repo() for _ in range(2)))
-        cls.repos = [client.post(REPOSITORY_PATH, body) for body in cls.bodies]
-        empty_dist = {'distributor_type_id': 'ostree_web_distributor',
-                      'config': {}}
+        """Create distributors with legal and illegal relative paths."""
+        super(CreateDistributorsTestCase, cls).setUpClass()
+        cls.responses = []
 
-        for repo in cls.repos:
-            cls.resources.add(repo.json()['_href'])  # mark for deletion
+        relative_paths = [_gen_rel_path(), _gen_rel_path(), _gen_rel_path(3)]
+        relative_paths.append(relative_paths[0])
+        relative_paths.append(relative_paths[0] + '/' + utils.uuid4())
+        relative_paths.append('/' + relative_paths[0])
 
-        # Create distributors to test whether their paths collide.
-        dist_1 = copy.deepcopy(empty_dist)
-        dist_2 = copy.deepcopy(empty_dist)
-        dist_3 = copy.deepcopy(empty_dist)
-        dist_4 = copy.deepcopy(empty_dist)
-        dist_5 = copy.deepcopy(empty_dist)
-        dist_1['distributor_config'] = {'relative_path': 'test/path'}
-        dist_2['distributor_config'] = {'relative_path': 'test/path'}
-        dist_3['distributor_config'] = {'relative_path': 'different/test/path'}
-        dist_4['distributor_config'] = {'relative_path': '/test/path'}
-        dist_5['distributor_config'] = {'relative_path': 'test/path/extended'}
-        repo1_url = urljoin(cls.repos[0].json()['_href'], 'distributors/')
-        repo2_url = urljoin(cls.repos[1].json()['_href'], 'distributors/')
-        cls.create_resp = client.post(repo1_url, dist_1)
-        cls.direct_conflict = client.post(repo2_url, dist_2)
-        cls.second_valid = client.post(repo2_url, dist_3)
-        cls.leading_slash = client.post(repo2_url, dist_4)
-        cls.sub_url = client.post(repo2_url, dist_5)
+        # Create two repositories
+        client = api.Client(cls.cfg, api.json_handler)
+        repos = [client.post(REPOSITORY_PATH, gen_repo()) for _ in range(2)]
+        for repo in repos:
+            cls.resources.add(repo['_href'])  # mark for deletion
 
-    def test_sanity(self):
-        """Ensure the first distributor was successful."""
-        self.assertEqual(self.create_resp.status_code, 201)
+        # Create a distributor for the first repository
+        client.response_handler = api.echo_handler
+        path = urljoin(repos[0]['_href'], 'distributors/')
+        body = _gen_distributor(relative_paths[0])
+        cls.responses.append(client.post(path, body))
 
-    def test_conflict(self):
-        """Ensure that a 400 is raised when there is a direct conflict."""
-        self.assertEqual(self.direct_conflict.status_code, 400)
+        # Create distributors for the second repository
+        path = urljoin(repos[1]['_href'], 'distributors/')
+        for relative_path in relative_paths[1:]:
+            body = _gen_distributor(relative_path)
+            cls.responses.append(client.post(path, body))
 
-    def test_second_valid(self):
-        """Create a second distributor with a nonconflicting relative_path."""
-        self.assertEqual(self.second_valid.status_code, 201)
+    def test_successes(self):
+        """Verify Pulp creates distributors when given good relative paths."""
+        for i, response in enumerate(self.responses[:3]):
+            with self.subTest(i=i):
+                self.assertEqual(response.status_code, 201)
 
-    def test_leading_slash(self):
-        """Ensure that a leading slash does not affect confict find."""
-        self.assertEqual(self.leading_slash.status_code, 400)
-
-    def test_sub_url_conflict_detection(self):
-        """Ensure that conflicts with sub urls are detected."""
-        self.assertEqual(self.sub_url.status_code, 400)
+    def test_failures(self):
+        """Verify Pulp doesn't create distributors when given bad rel paths."""
+        if selectors.bug_is_untestable(1106, self.cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/1106')
+        for i, response in enumerate(self.responses[3:]):
+            with self.subTest(i=i):
+                self.assertEqual(response.status_code, 400)
 
 
-class UpdateDistributors(utils.BaseAPITestCase):
+class UpdateDistributorsTestCase(utils.BaseAPITestCase):
     """Test the update of ostree distributors."""
 
     @classmethod
     def setUpClass(cls):
         """Create distributors and update with conflicting relative_paths."""
-        super(UpdateDistributors, cls).setUpClass()
-        if selectors.bug_is_untestable(1106):
-            cls.skipTest(cls, 'https://pulp.plan.io/issues/1106')
-        client = api.Client(cls.cfg, api.echo_handler)
-        cls.bodies = tuple((gen_repo() for _ in range(3)))
-        cls.repos = [client.post(REPOSITORY_PATH, body) for body in cls.bodies]
-        empty_dist = {'distributor_type_id': 'ostree_web_distributor',
-                      'config': {}}
+        super(UpdateDistributorsTestCase, cls).setUpClass()
 
-        for repo in cls.repos:
-            cls.resources.add(repo.json()['_href'])  # mark for deletion
+        # Create two repository + distributor pairs.
+        client = api.Client(cls.cfg, api.json_handler)
+        distributors = []
+        for _ in range(2):
+            repo = client.post(REPOSITORY_PATH, gen_repo())
+            cls.resources.add(repo['_href'])  # mark for deletion
+            distributors.append(client.post(
+                urljoin(repo['_href'], 'distributors/'),
+                _gen_distributor(_gen_rel_path()),
+            ))
 
-        # Create distributors to test whether their paths collide.
-        dist_1 = copy.deepcopy(empty_dist)
-        dist_2 = copy.deepcopy(empty_dist)
-        dist_3 = copy.deepcopy(empty_dist)
-        dist_1['distributor_config'] = {'relative_path': 'conflict/with/this'}
-        dist_2['distributor_config'] = {'relative_path': 'original/path'}
-        dist_3['distributor_config'] = {
-            'relative_path': 'another/original/path'
-        }
-        repo1_url = urljoin(cls.repos[0].json()['_href'], 'distributors/')
-        repo2_url = urljoin(cls.repos[1].json()['_href'], 'distributors/')
-        cls.create_resp_1 = client.post(repo1_url, dist_1)
-        cls.create_resp_2 = client.post(repo2_url, dist_2)
-        cls.create_resp_3 = client.post(repo2_url, dist_3)
-        dist_2_url = cls.create_resp_2.json()['_href']
-        dist_2['distributor_config']['relative_path'] = 'test/updated'
-        dist_3_url = cls.create_resp_3.json()['_href']
-        dist_3['distributor_config']['relative_path'] = 'conflict/with/this'
-        cls.update_resp_1 = client.put(dist_2_url, dist_2)
-        cls.update_resp_2 = client.put(dist_3_url, dist_3)
-        cls.get_update_succeed = client.get(dist_2_url)
-        cls.get_update_conflict = client.get(dist_3_url)
+        # Update the second distributor several times. After each update, we
+        # read the distributor. This extra read is necessary b/c the initial
+        # response is a call report.
+        cls.written_paths = (
+            _gen_rel_path(),  # successes
+            _gen_rel_path(3),
+            distributors[0]['config']['relative_path'],  # failures
+            distributors[0]['config']['relative_path'] + '/' + utils.uuid4(),
+            '/' + distributors[0]['config']['relative_path'],
+        )
+        cls.responses = []
+        cls.read_paths = []
+        for relative_path in cls.written_paths:
+            client.response_handler = api.echo_handler
+            cls.responses.append(client.put(
+                distributors[1]['_href'],
+                {'distributor_config': {'relative_path': relative_path}},
+            ))
+            tuple(api.poll_spawned_tasks(cls.cfg, cls.responses[-1].json()))
+            client.response_handler = api.json_handler
+            cls.read_paths.append(
+                client.get(distributors[1]['_href'])['config']['relative_path']
+            )
 
-    def test_creation(self):
-        """Ensure that distributors were created."""
-        self.assertEqual(self.create_resp_1.status_code, 201)
-        self.assertEqual(self.create_resp_2.status_code, 201)
-        self.assertEqual(self.create_resp_3.status_code, 201)
+    def test_status_codes(self):
+        """Assert all update requests return an HTTP 202, even if invalid."""
+        for i, response in enumerate(self.responses):
+            with self.subTest(i=i):
+                self.assertEqual(response.status_code, 202)
 
-    def test_update_creates_tasks(self):
-        """Ensure that updates return 202, even if they are invalid."""
-        self.assertEqual(self.update_resp_1.status_code, 202)
-        self.assertEqual(self.update_resp_2.status_code, 202)
+    def test_successes(self):
+        """Assert each valid update can be read back."""
+        for i in range(2):
+            with self.subTest(i=i):
+                self.assertEqual(self.written_paths[i], self.read_paths[i])
 
-    def test_get_update_succeed(self):
-        """Ensure that a valid config update actually updated the config."""
-        update_path = self.get_update_succeed.json()['config']['relative_path']
-        self.assertEqual(self.get_update_succeed.status_code, 200)
-        self.assertEqual(update_path, 'test/updated')
-
-    def test_get_update_conflict(self):
-        """Ensure that a conflicting config update does not update config."""
-        rel_path = self.get_update_conflict.json()['config']['relative_path']
-        self.assertEqual(self.get_update_succeed.status_code, 200)
-        self.assertEqual(rel_path, 'another/original/path')
+    def test_failures(self):
+        """Assert each invalid update cannot be read back."""
+        if selectors.bug_is_untestable(1106, self.cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/1106')
+        for i in range(2, len(self.written_paths)):
+            with self.subTest(i=i):
+                self.assertNotEqual(self.written_paths[i], self.read_paths[i])
