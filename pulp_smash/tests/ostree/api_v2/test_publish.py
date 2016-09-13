af5752fc922e29f5a72b33c836f2429632cccb82
Jeremy Audet <ichimonji10@gmail.com>
2016-09-13 13:57:02 -0400

    Add a test case that publishes an OSTree repo
    Add a new module, `pulp_smash.tests.ostree.api_v2.test_publish`. This
    module contains one test case, `PublishTestCase`, that creates, syncs
    and publishes a repository and makes some basic assertions about the
    process.
    
    Additional assertions about whether published files are accessible,
    whether the various publish options work, and whether published files
    are copied or hard-linked should also be written. But see:
    
    * https://pulp.plan.io/issues/2237
    * https://pulp.plan.io/issues/2254
    * https://pulp.plan.io/issues/2255

---
+++ pulp_smash/tests/ostree/api_v2/test_publish.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+"""Tests that publish OSTree repositories."""
+from __future__ import unicode_literals
+
+import unittest2
+
+from pulp_smash import api, config, utils
+from pulp_smash.compat import urljoin
+from pulp_smash.constants import OSTREE_BRANCH, OSTREE_FEED, REPOSITORY_PATH
+from pulp_smash.tests.ostree.utils import gen_distributor, gen_repo
+from pulp_smash.tests.ostree.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
+
+
+class PublishTestCase(unittest2.TestCase):
+    """Create, sync and publish an OSTree repository."""
+
+    def test_all(self):
+        """Create, sync and publish an OSTree repository.
+
+        Verify that:
+
+        * The distributor's ``last_publish`` attribute is ``None`` after the
+          sync. This demonstrates that ``auto_publish`` correctly defaults to
+          ``False``.
+        * The distributor's ``last_publish`` attribute is not ``None`` after
+          the publish.
+        """
+        cfg = config.get_config()
+        client = api.Client(cfg, api.json_handler)
+
+        # Create a repository.
+        body = gen_repo()
+        body['importer_config']['feed'] = OSTREE_FEED
+        body['importer_config']['branches'] = [OSTREE_BRANCH]
+        body['distributors'].append(gen_distributor())
+        repo = client.post(REPOSITORY_PATH, body)
+        self.addCleanup(client.delete, repo['_href'])
+
+        # Sync the repository.
+        utils.sync_repo(cfg, repo['_href'])
+        repo = client.get(repo['_href'], params={'details': True})
+        with self.subTest(comment='verify last_publish after sync'):
+            self.assertIsNone(repo['distributors'][0]['last_publish'])
+
+        # Publish the repository.
+        client.post(urljoin(repo['_href'], 'actions/publish/'), {
+            'id': repo['distributors'][0]['id'],
+        })
+        repo = client.get(repo['_href'], params={'details': True})
+        with self.subTest(comment='verify last_publish after publish'):
+            self.assertIsNotNone(repo['distributors'][0]['last_publish'])
--- pulp_smash/tests/ostree/utils.py
+++ pulp_smash/tests/ostree/utils.py
@@ -22,3 +22,25 @@ def gen_repo():
         'distributors': [],
         'notes': {'_repo-type': 'OSTREE'},
     }
+
+
+def gen_distributor():
+    """Return a semi-random dict for use in creating an OSTree distributor.
+
+    For more information, see the generic `repository CRUD`_ documentation and
+    the OSTree `distributor configuration`_ documentation.
+
+    .. NOTE: The OSTree `distributor configuration`_ documentation doesn't list
+        the available parameters. To discover which parameters are available,
+        execute ``pulp-admin -vv ostree repo create …``. See `Pulp #2254`_.
+
+    .. _repository CRUD:
+        http://docs.pulpproject.org/dev-guide/integration/rest-api/repo/cud.html
+    .. _distributor configuration:
+        http://docs.pulpproject.org/plugins/pulp_ostree/tech-reference/distributor.html
+    .. _Pulp #2254: https://pulp.plan.io/issues/2254
+    """
+    return {
+        'distributor_id': utils.uuid4(),
+        'distributor_type_id': 'ostree_web_distributor',
+    }
