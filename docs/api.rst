0d381841341b958ae23c53487962332845a5b2e7
Jeremy Audet <ichimonji10@gmail.com>
2016-10-11 12:29:15 -0400

    Refactor newly added Python sync tests
    Among other changes:
    
    * Conditionally skip portions of the new test based on
      https://pulp.plan.io/issues/1882 and https://pulp.plan.io/issues/2330.
    * Ensure the test case cleans up after itself.
    * Move the repository syncing logic into the first test, ensuring that
      the clean-up logic runs even when the sync fails.
    * Make docstrings valid.
    * Add the new module to the documentation.

--- docs/api.rst
+++ docs/api.rst
@@ -60,6 +60,7 @@ developers, not a gospel.
     api/pulp_smash.tests.python.api_v2
     api/pulp_smash.tests.python.api_v2.test_crud
     api/pulp_smash.tests.python.api_v2.test_duplicate_uploads
+    api/pulp_smash.tests.python.api_v2.test_sync_publish
     api/pulp_smash.tests.python.api_v2.utils
     api/pulp_smash.tests.python.utils
     api/pulp_smash.tests.rpm
--- pulp_smash/tests/python/api_v2/test_sync_publish.py
+++ pulp_smash/tests/python/api_v2/test_sync_publish.py
@@ -1,36 +1,39 @@
 # coding=utf-8
-"""Test the sync and publish API endpoints for Python `repositories`_.
-"""
+"""Test the sync and publish API endpoints for Python repositories."""
+from urllib.parse import urljoin
 
-from pulp_smash import api, constants, utils
-from pulp_smash.tests.python.api_v2 import utils as python_utils
+from packaging.version import Version
 
-REPO_CONTENT_SEARCH_URL = '/pulp/api/v2/repositories/{repo}/search/units/'
+from pulp_smash import api, constants, selectors, utils
+from pulp_smash.tests.python.api_v2.utils import gen_repo
+from pulp_smash.tests.python.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
 
 
-class SyncPythonRepoTestCase(utils.BaseAPITestCase):
+class SyncTestCase(utils.BaseAPITestCase):
     """If a valid feed is given, the sync completes without reported errors."""
 
     @classmethod
     def setUpClass(cls):
         """Create a Python repo and sync it."""
-        super(SyncPythonRepoTestCase, cls).setUpClass()
+        super(SyncTestCase, cls).setUpClass()
         client = api.Client(cls.cfg, api.json_handler)
-        body = python_utils.gen_repo()
-        imp_config = {'feed': constants.PYTHON_FEED_URL,
-                      'package_names': 'shelf-reader'}
-        body['importer_config'] = imp_config
-        cls.repo_id = body.get('id')
+        body = gen_repo()
+        body['importer_config'] = {
+            'feed': constants.PYTHON_FEED_URL,
+            'package_names': 'shelf-reader',
+        }
         cls.repo_href = client.post(constants.REPOSITORY_PATH, body)['_href']
-        cls.report = utils.sync_repo(cls.cfg, cls.repo_href)
+        cls.resources.add(cls.repo_href)
 
-    def test_start_sync_code(self):
-        """Assert the call to sync a repository returns an HTTP 202."""
-        self.assertEqual(self.report.status_code, 202)
+    def test_01_sync(self):
+        """Sync the repository.
 
-    def test_task_progress_report(self):
-        """Assert no task's progress report contains error details."""
-        tasks = tuple(api.poll_spawned_tasks(self.cfg, self.report.json()))
+        Assert the call to sync the repository returns an HTTP 202. Assert none
+        of the task reports contain error details.
+        """
+        report = utils.sync_repo(self.cfg, self.repo_href)
+        self.assertEqual(report.status_code, 202)
+        tasks = tuple(api.poll_spawned_tasks(self.cfg, report.json()))
         for i, task in enumerate(tasks):
             step_reports = task['progress_report']['python_importer']
             for step in step_reports:
@@ -38,14 +41,21 @@ class SyncPythonRepoTestCase(utils.BaseAPITestCase):
                     error_details = step['error_details']
                     self.assertEqual(error_details, [], task)
 
-    def test_multiple_types(self):
-        """Verify that we synced the correct number units of each type."""
-        repo_search_url = REPO_CONTENT_SEARCH_URL.format(repo=self.repo_id)
-        content = api.Client(self.cfg).post(repo_search_url,
-                                            {'criteria': {}}).json()
+    def test_02_package_types(self):
+        """Assert sdist and bdist_wheel versions of the package were synced.
 
-        self.assertEqual(len(content), 2)
-        types_in_repo = [content[1]['metadata']['packagetype']]
-        types_in_repo.append(content[0]['metadata']['packagetype'])
-        self.assertTrue('sdist' in types_in_repo)
-        self.assertTrue('bdist_wheel' in types_in_repo)
+        This test targets:
+
+        * `Pulp issue #1882 <https://pulp.plan.io/issues/1882>`_
+        * `Pulp issue #2330 <https://pulp.plan.io/issues/2330>`_
+        """
+        if self.cfg.version < Version('2.11'):
+            self.skipTest('https://pulp.plan.io/issues/1882')
+        if selectors.bug_is_untestable(2230, self.cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/2330')
+        units = api.Client(self.cfg).post(
+            urljoin(self.repo_href, 'search/units/'),
+            {'criteria': {}},
+        ).json()
+        unit_types = {unit['metadata']['packagetype'] for unit in units}
+        self.assertEqual(unit_types, {'sdist', 'bdist_wheel'})
