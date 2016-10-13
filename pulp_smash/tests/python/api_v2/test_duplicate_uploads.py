e010118c522359c18a8d6771aa83a5b7067ff52d
Jeremy Audet <ichimonji10@gmail.com>
2016-10-13 11:28:59 -0400

    Skip test affected by Pulp issue 2334
    See: https://pulp.plan.io/issues/2334

--- pulp_smash/tests/python/api_v2/test_duplicate_uploads.py
+++ pulp_smash/tests/python/api_v2/test_duplicate_uploads.py
@@ -14,7 +14,11 @@ The second upload should silently fail for all Pulp releases in the 2.x series.
 .. _Pulp #1406: https://pulp.plan.io/issues/1406
 .. _Pulp Smash #81: https://github.com/PulpQE/pulp-smash/issues/81
 """
-from pulp_smash import api, utils
+import unittest
+
+from packaging.version import Version
+
+from pulp_smash import api, selectors, utils
 from pulp_smash.constants import PYTHON_EGG_URL, REPOSITORY_PATH
 from pulp_smash.tests.python.api_v2.utils import gen_repo
 from pulp_smash.tests.python.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
@@ -29,6 +33,9 @@ class DuplicateUploadsTestCase(
     def setUpClass(cls):
         """Create a Python repo. Upload a Python package into it twice."""
         super(DuplicateUploadsTestCase, cls).setUpClass()
+        if (cls.cfg.version >= Version('2.11') and
+                selectors.bug_is_untestable(2334, cls.cfg.version)):
+            raise unittest.SkipTest('https://pulp.plan.io/issues/2334')
         unit = utils.http_get(PYTHON_EGG_URL)
         unit_type_id = 'python_package'
         client = api.Client(cls.cfg, api.json_handler)
