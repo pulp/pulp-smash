efaffb64b53867e276320c723a6cca315d3f4413
Jeremy Audet <ichimonji10@gmail.com>
2017-03-09 06:09:38 +0000

    Deduplicate two test cases for updateinfo.xml
    The following test cases verify a published repository's
    `updateinfo.xml` file:
    
    * pulp_smash.tests.rpm.api_v2.test_updateinfo.UpdateInfoTestCase
    * pulp_smash.tests.rpm.api_v2.test_upload_publish.UploadErratumTestCase
    
    These test cases are redundant. They both upload an errata (or two) to a
    repository, publish that repository, and verify that the published
    `updateinfo.xml` file has the correct contents. Deduplicate them. Drop
    `UploadErratumTestCase` and keep `UpdateInfoTestCase`.

--- pulp_smash/tests/rpm/utils.py
+++ pulp_smash/tests/rpm/utils.py
@@ -1,12 +1,8 @@
 # coding=utf-8
 """Utilities for RPM tests."""
-import json
-
-import requests
 from packaging.version import Version
 
-from pulp_smash import cli, config, selectors, utils
-from pulp_smash.constants import RPM_ERRATUM_URL
+from pulp_smash import cli, selectors, utils
 
 
 def set_up_module():
@@ -15,25 +11,6 @@ def set_up_module():
     See :mod:`pulp_smash.tests` for more information.
     """
     utils.skip_if_type_is_unsupported('rpm')
-
-
-def gen_erratum():
-    """Return an erratum with a randomized ID.
-
-    Fetch, decode, munge and return the erratum file at
-    :data:`pulp_smash.constants.RPM_ERRATUM_URL`. This erratum can be uploaded
-    and imported into an RPM repository with
-    :meth:`pulp_smash.utils.upload_import_erratum`.
-    """
-    response = requests.get(RPM_ERRATUM_URL)
-    response.raise_for_status()
-    erratum = json.loads(response.text)
-    erratum['id'] = utils.uuid4()
-    # "Cannot provide multiple checksums when uploading an erratum"
-    if selectors.bug_is_untestable(2020, config.get_config().version):
-        for package in erratum['pkglist'][0]['packages']:
-            package['sum'] = package['sum'][:2]
-    return erratum
 
 
 def check_issue_2277(cfg):
