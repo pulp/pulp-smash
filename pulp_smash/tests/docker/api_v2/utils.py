68b966df6bbd53bb84d9a08fa591c342355373d7
Jeremy Audet <ichimonji10@gmail.com>
2017-07-28 13:59:01 -0400

    Move Docker tests into more appropriate location
    Most of the tests in `pulp_smash.tests.docker.cli.test_sync_publish` use
    Pulp's CLI and API interfaces. Rewrite the tests to use just Pulp's API,
    and move them to `pulp_smash.tests.docker.api_v2.test_sync_publish`.

--- pulp_smash/tests/docker/api_v2/utils.py
+++ pulp_smash/tests/docker/api_v2/utils.py
@@ -6,7 +6,18 @@ from pulp_smash import utils
 def gen_repo():
     """Return a semi-random dict that used for creating a Docker repo."""
     return {
-        'id': utils.uuid4(), 'importer_config': {},
+        'id': utils.uuid4(),
+        'importer_config': {},
         'importer_type_id': 'docker_importer',
         'notes': {'_repo-type': 'docker-repo'},
+    }
+
+
+def gen_distributor():
+    """Return a semi-random dict for use in creating a Docker distributor."""
+    return {
+        'auto_publish': False,
+        'distributor_config': {},
+        'distributor_id': utils.uuid4(),
+        'distributor_type_id': 'docker_distributor_web',
     }
