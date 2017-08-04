f35702e2e1169dfd31793be68b7bf54af49b4bf6
Jeremy Audet <ichimonji10@gmail.com>
2017-08-04 12:15:21 -0400

    Fiddle with newly-added OSTree copy tests
    These changes could have been requested on the pull request itself.
    However, it's faster to simply make these changes myself.
    
    See: https://github.com/PulpQE/pulp-smash/pull/712
    
    See: 1b1caa63cecf361f8feec379c61f59210b0a0d61

--- pulp_smash/tests/ostree/api_v2/test_copy.py
+++ pulp_smash/tests/ostree/api_v2/test_copy.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-"""Copy content between OSTree repositories with a filter."""
+"""Tests that copy content between OSTree repositories."""
 import random
 import unittest
 from urllib.parse import urljoin
@@ -10,47 +10,41 @@ from pulp_smash.tests.ostree.utils import gen_distributor, gen_repo
 from pulp_smash.tests.ostree.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
 
 
-class CopyTestCase(unittest.TestCase):
+class FilterTestCase(unittest.TestCase):
     """Copy content between OSTree repositories with a filter."""
 
     def test_all(self):
-        """Copy content between OSTree repositories with a filter..
-
-        Verify that filters can be used to restrict which units
-        are copied between repositories.
+        """Copy content between OSTree repositories with a filter.
 
         Do the following:
 
         1. Create a pair of repositories, and populate the first.
         2. Randomly select a unit from the first repository, and copy
-            it to the second repository.
+           it to the second repository.
         3. Verify that the selected unit is the only one in the second
-            repository.
-
+           repository.
         """
-        repos = []  # list to store data related to created repos
+        repos = []
         cfg = config.get_config()
         client = api.Client(cfg, api.json_handler)
 
-        # Create the 1st repository with at least 2 content units
+        # Create and populate a source repository.
         body = gen_repo()
         body['importer_config']['feed'] = OSTREE_FEED
         body['importer_config']['branches'] = OSTREE_BRANCHES
-        body['distributors'].append(gen_distributor())
+        body['distributors'] = [gen_distributor()]
         repos.append(client.post(REPOSITORY_PATH, body))
         self.addCleanup(client.delete, repos[0]['_href'])
-
-        # Sync the 1st repository
         utils.sync_repo(cfg, repos[0])
-        src_unit_id = random.choice(
-            utils.search_units(cfg, repos[0]))['metadata']['_id']
 
-        # Create the 2nd repository
-        body = gen_repo()
-        repos.append(client.post(REPOSITORY_PATH, body))
+        # Create a destination repository.
+        repos.append(client.post(REPOSITORY_PATH, gen_repo()))
         self.addCleanup(client.delete, repos[1]['_href'])
 
-        # Copy random unit from the 1st repo to 2nd one.
+        # Copy a random unit between the repos, and verify the result.
+        src_unit_id = random.choice(
+            utils.search_units(cfg, repos[0])
+        )['metadata']['_id']
         client.post(urljoin(repos[1]['_href'], 'actions/associate/'), {
             'source_repo_id': repos[0]['id'],
             'criteria': {
@@ -58,14 +52,8 @@ class CopyTestCase(unittest.TestCase):
                 'type_ids': ['ostree'],
             },
         })
-
-        # Search for random copied unit on 2nd repo
         dst_unit_ids = [
             unit['metadata']['_id'] for unit in
-            utils.search_units(cfg, repos[1], {
-                'filters': {}, 'type_ids': ['ostree']
-            })
+            utils.search_units(cfg, repos[1], {'type_ids': ['ostree']})
         ]
-
-        # Assert unit from 1st repo was copied to 2nd
         self.assertEqual([src_unit_id], dst_unit_ids)
