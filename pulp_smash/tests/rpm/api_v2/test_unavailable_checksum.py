d7540feec8a363c419a488abd68a3ab6631b4525
Jeremy Audet <ichimonji10@gmail.com>
2017-08-11 15:11:40 -0400

    Refactor new test case for publish checksum types
    Refactor newly added `UnavailableChecksumTestCase`. See:
    82bf3446f8bc9ba0e2568b9d44da19cf7be081de

--- pulp_smash/tests/rpm/api_v2/test_unavailable_checksum.py
+++ pulp_smash/tests/rpm/api_v2/test_unavailable_checksum.py
@@ -1,118 +1,73 @@
 # coding=utf-8
-"""Test to publish repo with lazy content and unavailable checksum type."""
-from pulp_smash import api, utils
+"""Tests that publish repositories with unavailable checksum types."""
+import unittest
+
+from pulp_smash import api, config, utils
 from pulp_smash.constants import (
+    DRPM_UNSIGNED_FEED_URL,
     REPOSITORY_PATH,
     RPM_UNSIGNED_FEED_URL,
-    DRPM_UNSIGNED_FEED_URL,
     SRPM_UNSIGNED_FEED_URL,
-
 )
 from pulp_smash.exceptions import TaskReportError
-from pulp_smash.tests.rpm.api_v2.utils import (
-    gen_distributor,
-    gen_repo,
-)
-from pulp_smash.tests.rpm.utils import set_up_module
+from pulp_smash.tests.rpm.api_v2.utils import gen_distributor, gen_repo
+from pulp_smash.tests.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
 
 
-def setUpModule():  # pylint:disable=invalid-name
-    """Skip tests if the RPM plugin is not installed."""
-    set_up_module()
+class UnavailableChecksumTestCase(unittest.TestCase):
+    """Publish a lazily-synced repository with unavailable checksum types."""
 
+    def test_rpm(self):
+        """Publish a RPM repo. See :meth:`do_test`."""
+        self.do_test(RPM_UNSIGNED_FEED_URL, 'rpm')
 
-class UnavailableChecksumTestCase(utils.BaseAPITestCase):
-    """Ensure that an exception is raised.
+    def test_srpm(self):
+        """Publish a SRPM repo. See :meth:`do_test`."""
+        self.do_test(SRPM_UNSIGNED_FEED_URL, 'srpm')
 
-    Assert that an exception is raised when publishing an RPM, DRPM, or SRPM
-    repo using an unavailable checksum type.
+    def test_drpm(self):
+        """Publish a DRPM repo. See :meth:`do_test`."""
+        self.do_test(DRPM_UNSIGNED_FEED_URL, 'drpm')
 
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """Provide server config for test execution.
+    def do_test(self, feed, type_id):
+        """Publish a repository with an invalid checksum.
 
         Do the following:
 
-        1. Reset Pulp, including the Squid cache.
-        2. Create a Client.
+        1. Create and sync repository of type ``type_id`` and with the given
+           feed. Ensure the repository's importer has a deferred/lazy download
+           policy.
+        2. Determine which checksum type the units in the repository use. (As
+           of this writing, Pulp can handle md5, sha1 and sha256 checksum
+           types.)
+        3. Publish the repository with checksum types different from what the
+           units in the repository use. Assert the publish fails.
 
+        This test targets `Pulp Smash #287
+        <https://github.com/PulpQE/pulp-smash/issues/287>`_.
         """
-        super(UnavailableChecksumTestCase, cls).setUpClass()
+        cfg = config.get_config()
+        client = api.Client(cfg, api.json_handler)
 
-        # Ensure `locally_stored_units` is 0 before we start.
-        utils.reset_squid(cls.cfg)
-        utils.reset_pulp(cls.cfg)
-
-        cls.client = api.Client(cls.cfg, api.json_handler)
-
-    def create_sync_repo(self, feed):
-        """Create and sync a repository, using on_demand download policy."""
+        # Create and sync a repository.
         body = gen_repo()
         body['importer_config']['download_policy'] = 'on_demand'
         body['importer_config']['feed'] = feed
-        distributor = gen_distributor()
-        body['distributors'] = [distributor]
+        body['distributors'] = [gen_distributor()]
+        repo = client.post(REPOSITORY_PATH, body)
+        self.addCleanup(client.delete, repo['_href'])
+        utils.sync_repo(cfg, repo)
+        repo = client.get(repo['_href'], params={'details': True})
 
-        # Create repo
-        repo = self.client.post(REPOSITORY_PATH, body)
-        self.addCleanup(self.client.delete, repo['_href'])
+        # Figure out which checksum type the units in the repository use.
+        units = utils.search_units(cfg, repo, {'type_ids': type_id})
+        checksums = {'md5', 'sha1', 'sha256'}
 
-        # Sync repo
-        utils.sync_repo(self.cfg, repo)
-
-        # Return info related to repo, and distributor
-        repo = self.client.get(repo['_href'], params={'details': True})
-        return repo, distributor
-
-    def test_all(self):
-        """Publishing a repo with an invalid checksum should throw an error.
-
-        1. Create a repository - RPM, SRPM, and DRPM with the "on demand"
-        download policy.
-        2. Sync the repository.
-        3. Get checksum type of an unit.
-        4. Update the checksum type.
-        5. Attempt to publish the repo with the new checksum.
-        6. Assert that an Exception is raised.
-
-        """
-        variants = (
-            (RPM_UNSIGNED_FEED_URL, 'rpm'),
-            (DRPM_UNSIGNED_FEED_URL, 'drpm'),
-            (SRPM_UNSIGNED_FEED_URL, 'srpm'),
-        )
-
-        for feed, type_id in variants:
-            with self.subTest(
-                comment='publish unavailable checksum',
-                type_id=type_id
-            ):
-
-                repo, distributor = self.create_sync_repo(feed)
-                units = utils.search_units(
-                    self.cfg, repo,
-                    {'type_ids': type_id}
-                )
-
-                # Get a checksum type different from the received one
-                checksum_type = {'md5', 'sha1', 'sha256'}
-                checksum_type = checksum_type.difference(
-                    {units[0]['metadata']['checksumtype']}
-                )
-
-                for checksum in checksum_type:
-                    # Update the new checksum type
-                    self.client.put(repo['_href'], {
-                        'distributor_configs': {
-                            distributor['distributor_id']: {
-                                'checksum_type': checksum,
-                            }
-                        }
-                    })
-
-                    # Attempt to publish the repo with the new checksum type
-                    with self.assertRaises(TaskReportError):
-                        # It is expected an Exception to be raised
-                        utils.publish_repo(self.cfg, repo)
+        # Publish this repo with checksums that aren't available.
+        checksums -= {units[0]['metadata']['checksumtype']}
+        for checksum in checksums:
+            client.put(repo['distributors'][0]['_href'], {
+                'distributor_config': {'checksum_type': checksum}
+            })
+            with self.assertRaises(TaskReportError):
+                utils.publish_repo(cfg, repo)
