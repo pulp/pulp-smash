8157631db1ffbf86d0c10530198b69f418c8a018
Jeremy Audet <ichimonji10@gmail.com>
2017-10-23 16:44:23 -0400

    Update just-added tests for sqlite file mtimes
    The previous commit added a test case that, in short, verified that file
    mtimes were left unchanged in certain circumstances. Update this test
    case by doing the following:
    
    * Correct an inappropriate call to `PulpSmashConfig.get_systems`.
    * Avoid a call to `ls`, whose format is liable to change over time and
      across varying distributions. Use `stat` instead, whose output may be
      tightly controlled.
    * Move this test case into an existing module, instead of letting a new
      module be created. While there's no hard-and-fast rule about when to
      create a new module or when to add to an existing one, the new module
      name (`test_copy_mtime`) seems extremely specific and unlikely to be
      filled with more test cases, and the new test case fits reasonably
      well into an existing module.

--- docs/api/pulp_smash.tests.rpm.api_v2.test_copy_mtime.rst
+++
@@ -1,6 +0,0 @@
-`pulp_smash.tests.rpm.api_v2.test_copy_mtime`
-=============================================
-
-Location: :doc:`/index` → :doc:`/api` → :doc:`/api/pulp_smash.tests.rpm.api_v2.test_copy_mtime`
-
-.. automodule:: pulp_smash.tests.rpm.api_v2.test_copy_mtime
--- pulp_smash/tests/rpm/api_v2/test_copy.py
+++ pulp_smash/tests/rpm/api_v2/test_copy.py
@@ -1,12 +1,21 @@
 # coding=utf-8
-"""Tests for copying RPM units between repositories."""
+"""Test cases that copy content units."""
+import os
+import time
 import unittest
 from urllib.parse import urljoin
 
-from pulp_smash import api, config, selectors, utils
-from pulp_smash.constants import REPOSITORY_PATH, RPM_UPDATED_INFO_FEED_URL
+from pulp_smash import api, cli, config, selectors, utils
+from pulp_smash.constants import (
+    REPOSITORY_PATH,
+    RPM_SIGNED_URL,
+    RPM_UNSIGNED_FEED_URL,
+    RPM_UPDATED_INFO_FEED_URL,
+)
 from pulp_smash.tests.rpm.api_v2.utils import gen_distributor, gen_repo
 from pulp_smash.tests.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
+
+_PATH = '/var/lib/pulp/published/yum/https/repos/'
 
 
 class CopyErrataRecursiveTestCase(unittest.TestCase):
@@ -56,3 +65,63 @@ class CopyErrataRecursiveTestCase(unittest.TestCase):
         # Assert that RPM packages were copied.
         units = utils.search_units(cfg, repos[1], {'type_ids': ['rpm']})
         self.assertGreater(len(units), 0)
+
+
+class MtimeTestCase(unittest.TestCase):
+    """Test whether copied files retain their original mtime."""
+
+    def test_all(self):
+        """Test whether copied files retain their original mtime.
+
+        This test targets the following issues:
+
+        * `Pulp #2783 <https://pulp.plan.io/issues/2783>`_
+        * `Pulp Smash #720 <https://github.com/PulpQE/pulp-smash/issues/720>`_
+
+        Do the following:
+
+        1. Create, sync and publish a repository, with ``generate_sqlite`` set
+           to true.
+        2. Get the ``mtime`` of the sqlite files.
+        3. Upload an RPM package into the repository, and sync the repository.
+        4. Get the ``mtime`` of the sqlite files again. Verify that the mtimes
+           are the same.
+        """
+        cfg = config.get_config()
+        if selectors.bug_is_untestable(2783, cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/2783')
+
+        # Create, sync and publish a repository.
+        client = api.Client(cfg, api.json_handler)
+        body = gen_repo()
+        body['importer_config']['feed'] = RPM_UNSIGNED_FEED_URL
+        body['distributors'] = [gen_distributor()]
+        body['distributors'][0]['distributor_config']['generate_sqlite'] = True
+        repo = client.post(REPOSITORY_PATH, body)
+        self.addCleanup(client.delete, repo['_href'])
+        repo = client.get(repo['_href'], params={'details': True})
+        utils.sync_repo(cfg, repo)
+        utils.publish_repo(cfg, repo)
+
+        # Get the mtime of the sqlite files.
+        cli_client = cli.Client(cfg, cli.echo_handler)
+        cmd = "stat --format %Y '{}'/*".format(os.path.join(
+            _PATH,
+            repo['distributors'][0]['config']['relative_url'],
+            'repodata',
+        ))
+        mtimes_pre = (
+            cli_client.machine.session().run(cmd)[1].strip().split().sort()
+        )
+
+        # Upload to the repo, and sync it.
+        rpm = utils.http_get(RPM_SIGNED_URL)
+        utils.upload_import_unit(cfg, rpm, {'unit_type_id': 'rpm'}, repo)
+        utils.sync_repo(cfg, repo)
+
+        # Get the mtime of the sqlite files again.
+        time.sleep(1)
+        mtimes_post = (
+            cli_client.machine.session().run(cmd)[1].strip().split().sort()
+        )
+        self.assertEqual(mtimes_pre, mtimes_post)
