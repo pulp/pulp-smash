pulp/pulp-smash

commit f05212a564c787dfb2dd5c43058964d94fdd96d4

Update new test that installs an RPM erratum
Module `pulp_smash.tests.pulp2.rpm.api_v2.test_errata` was just added.
It currently contains a single test case that applies an erratum and
verifies that the associated RPM is installed. Update the test case:

* `PackageManager.update` expects positional arguments, such as
  `update('walrus', 'bear')` or `update(*my_tuple)`. Fix the calls to
  this method.
* Much of the logic in the test case can be grouped into highly
  independent methods. Do so.
* Make function `get_rpms_names_versions` use Pulp's API instead of its
  CLI. The API's output is much more stable.
* Many Pulp 2 constants recently moved to
  `pulp_smash.tests.pulp2.constants`. Fix a reference to one of them.

See: aa7e2a13aa5cd770ee7369caab4ab0451baf0d5

pulp_smash/tests/pulp2/rpm/api_v2/test_errata.py
@@ -1,9 +1,10 @@
-"""Test whether applying an ``Errata`` will update an RPM package version."""
+"""Test whether applying an erratum will update an RPM package version."""
 import unittest
 from urllib.parse import urljoin
 
 from pulp_smash import api, cli, config, utils
-from pulp_smash.constants import REPOSITORY_PATH, RPM_UNSIGNED_FEED_URL
+from pulp_smash.constants import RPM_UNSIGNED_FEED_URL
+from pulp_smash.tests.pulp2.constants import REPOSITORY_PATH
 from pulp_smash.tests.pulp2.rpm.api_v2.utils import gen_distributor, gen_repo
 from pulp_smash.tests.pulp2.rpm.utils import (
     gen_yum_config_file,
@@ -12,33 +13,50 @@ from pulp_smash.tests.pulp2.rpm.utils import (
 from pulp_smash.tests.pulp2.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
 
 
-class ErrataLatestPackageVersionTestCase(unittest.TestCase):
-    """Test whether applying an Errata will update a RPM package version."""
+class ApplyErratumTestCsae(unittest.TestCase):
+    """Test whether applying an erratum will install referenced RPMs."""
 
     def test_all(self):
-        """Test whether applying an Errata will update a RPM package version.
+        """Test whether applying an erratum will install referenced RPMs.
 
-        This test targets the following issue:
+        It does the following:
 
-        * `Pulp Smash #760 <https://github.com/PulpQE/pulp-smash/issues/760>`_
+        1. Create, sync and publish a repository with errata and RPM packages.
+           Two of these RPMs have the same name and different versions. The
+           newest version is referenced by one of the errata.
+        2. Install the older version of the aforementioned RPM.
+        3. Apply the erratum, and verify that newer version of the RPM is
+           installed.
 
-        Do the following:
-
-        1. Create, sync and publish a repository with ``Errata``, and RPM
-           packages.
-           Two of these RPMs have the **same name**, and **different
-           versions**.
-           The newest version is part of the ``Errata``.
-        2. Install the oldest version of the aforementioned RPM.
-        3. Apply the ``Errata``, and verify that latest version of previous
-           installed package was updated properly.
+        This test targets `Pulp Smash #760
+        <https://github.com/PulpQE/pulp-smash/issues/760>`_.
         """
-        # Create, sync and publish a repository.
         cfg = config.get_config()
-        client = api.Client(cfg, api.json_handler)
+        repo = self._create_sync_publish_repo(cfg)
+        self._create_repo_file(cfg, repo)
+
+        # Install the older version of the RPM, and verify it's been installed.
+        rpm_name = 'walrus'
+        rpm_versions = get_rpm_names_versions(cfg, repo)[rpm_name]
         pkg_mgr = cli.PackageManager(cfg)
-        sudo = '' if utils.is_root(cfg) else 'sudo '
-        verify = cfg.get_systems('api')[0].roles['api'].get('verify')
+        pkg_mgr.install((rpm_name + '-' + rpm_versions[0]))
+        self.addCleanup(pkg_mgr.uninstall, rpm_name)
+        cli_client = cli.Client(cfg)
+        rpm = cli_client.run(('rpm', '-q', rpm_name)).stdout.strip()
+        self.assertTrue(rpm.startswith('-'.join((rpm_name, rpm_versions[0]))))
+
+        # Apply erratum. Verify that the newer version of the RPM is installed.
+        pkg_mgr.upgrade(self._get_upgrade_targets(cfg))
+        rpm = cli_client.run(('rpm', '-q', rpm_name)).stdout.strip()
+        self.assertTrue(rpm.startswith('-'.join((rpm_name, rpm_versions[1]))))
+
+    def _create_sync_publish_repo(self, cfg):
+        """Create, sync and publish a repository.
+
+        Also, schedule it for deletion. Return a detailed dict of information
+        about the repository.
+        """
+        client = api.Client(cfg, api.json_handler)
         body = gen_repo()
         body['importer_config']['feed'] = RPM_UNSIGNED_FEED_URL
         body['distributors'] = [gen_distributor()]
@@ -47,41 +65,42 @@ class ErrataLatestPackageVersionTestCase(unittest.TestCase):
         repo = client.get(repo['_href'], params={'details': True})
         utils.sync_repo(cfg, repo)
         utils.publish_repo(cfg, repo)
+        return repo
 
-        # Pick a RPM with two versions.
-        rpm_name = 'walrus'
-        rpm_versions = get_rpm_names_versions(cfg, repo['id'])[rpm_name]
-        cli_client = cli.Client(
-            cfg,
-            cli.code_handler,
-            pulp_system=cfg.get_systems('shell')[0]
-        )
+    def _create_repo_file(self, cfg, repo):
+        """Create a file in ``/etc/yum.repos.d/`` referencing the repository.
+
+        Also, schedule it for deletion. Return nothing.
+        """
+        verify = cfg.get_systems('api')[0].roles['api'].get('verify')
+        sudo = () if utils.is_root(cfg) else ('sudo',)
         repo_path = gen_yum_config_file(
             cfg,
-            baseurl=urljoin(
-                cfg.get_base_url(),
-                ('pulp/repos/' +
-                 repo['distributors'][0]['config']['relative_url'])
-            ),
+            baseurl=urljoin(cfg.get_base_url(), urljoin(
+                'pulp/repos/',
+                repo['distributors'][0]['config']['relative_url']
+            )),
             enabled=1,
             gpgcheck=0,
             metadata_expire=0,  # force metadata to load every time
             repositoryid=repo['id'],
             sslverify='yes' if verify else 'no',
         )
-        self.addCleanup(
-            cli_client.run,
-            '{}rm {}'.format(sudo, repo_path).split()
-        )
+        self.addCleanup(cli.Client(cfg).run, sudo + ('rm', repo_path))
 
-        # Install old version of package on a host.
-        pkg_mgr.install((rpm_name + '-' + rpm_versions[0]))
-        self.addCleanup(pkg_mgr.uninstall, rpm_name)
-        cli_client.run(('rpm', '-q', (rpm_name + '-' + rpm_versions[0])))
-
-        # Apply Errata.
-        pkg_mgr.upgrade(('--advisory', 'RHEA-2012:0055'))
-        rpm = cli_client.run(('rpm', '-q', rpm_name))
-
-        # Verify that the latest version of package was updated.
-        self.assertIn(rpm_versions[1], rpm.stdout)
+    def _get_upgrade_targets(self, cfg):
+        """Get a tuple of upgrade targets for erratum RHEA-2012:0055."""
+        erratum = 'RHEA-2012:0055'
+        # Many of Pulp Smash's classes are concerned with hiding the
+        # differences between different target platforms. That's the point of
+        # methods like PackageManager.upgrade. But sometimes, we need to peek
+        # under the hood, and this is a good example of that. Maybe
+        # _get_package_manager() could be made into a public function.
+        yum_or_dnf = cli.PackageManager._get_package_manager(cfg)  # noqa pylint:disable=protected-access
+        self.assertIn(yum_or_dnf, ('yum', 'dnf'))
+        if yum_or_dnf == 'yum':
+            return ('--advisory', erratum)
+        lines = cli.Client(cfg).run((
+            'dnf', '--quiet', 'updateinfo', 'list', erratum
+        )).stdout.strip().splitlines()
+        return tuple((line.split()[2] for line in lines))

pulp_smash/tests/pulp2/rpm/utils.py
@@ -146,29 +146,21 @@ def gen_yum_config_file(cfg, repositoryid, **kwargs):
     return path
 
 
-def get_rpm_names_versions(server_config, repo_id):
-    """Get a dict of repo's RPMs with names as keys, mapping to version lists.
+def get_rpm_names_versions(cfg, repo):
+    """Get a dict of a repository's RPMs and their versions.
 
-    :param pulp_smash.config.PulpSmashConfig server_config: Information about
-        the Pulp deployment being targeted.
-    :param repo_id: A RPM repository ID.
+    :param pulp_smash.config.PulpSmashConfig cfg: Information about a Pulp app.
+    :param repo: A dict of information about a repository.
     :returns: The name and versions of each package in the repository, with the
         versions sorted in ascending order. For example: ``{'walrus': ['0.71',
         '5.21']}``.
     """
-    keyword = 'Filename:'
-    completed_proc = cli.Client(server_config).run(
-        'pulp-admin rpm repo content rpm --repo-id {}'.format(repo_id).split()
-    )
-    rpms = {}
-    for line in completed_proc.stdout.splitlines():
-        if keyword not in line:
-            continue
-        # e.g. 'Filename: my-walrus-0.71-1.noarch.rpm ' → ['my-walrus', '0.71']
-        filename_parts = line.lstrip(keyword).strip().split('-')[:-1]
-        name = '-'.join(filename_parts[:-1])
-        version = filename_parts[-1]
-        rpms.setdefault(name, []).append(version)
+    rpms = utils.search_units(cfg, repo, {'type_ids': ['rpm']})
+    names_versions = {}
     for rpm in rpms:
-        rpms[rpm] = sorted(rpms[rpm], key=Version)
-    return rpms
+        rpm_name = rpm['metadata']['name']
+        names_versions.setdefault(rpm_name, [])
+        names_versions[rpm_name].append(rpm['metadata']['version'])
+    for versions in names_versions.values():
+        versions.sort()
+    return names_versions

commit 39241e792ba72cf261af8e1d4deb5b024390163e

Fix functions for getting RPM names and versions
The just-added `get_rpm_names_versions()` function communicates with
Pulp via its API, and it requires API-specific information, like a
repository's href. As a result, this function most useful when called
from API tests. It's not easy to robustly call this function from CLI
tests. Move this function into
`pulp_smash.tests.pulp2.rpm.api_v2.utils`, in order to better indicate
its intended use case. The function also incorrectly sorts version
numbers. The following function calls produce differing results:

    from packaging.version import Version
    sorted(['0.2', '0.10'])
    sorted(['0.2', '0.10'], key=Version)

Fix the function so that it correctly sorts version numbers.

Reinstate a CLI equivalent to `get_rpm_names_versions()`. (With slightly
improved version sorting logic.)

This commit fixes errors introduced by
f05212a564c787dfb2dd5c43058964d94fdd96d4

pulp_smash/tests/pulp2/rpm/api_v2/test_errata.py
@@ -5,11 +5,12 @@ from urllib.parse import urljoin
 from pulp_smash import api, cli, config, utils
 from pulp_smash.constants import RPM_UNSIGNED_FEED_URL
 from pulp_smash.tests.pulp2.constants import REPOSITORY_PATH
-from pulp_smash.tests.pulp2.rpm.api_v2.utils import gen_distributor, gen_repo
-from pulp_smash.tests.pulp2.rpm.utils import (
-    gen_yum_config_file,
+from pulp_smash.tests.pulp2.rpm.api_v2.utils import (
+    gen_distributor,
+    gen_repo,
     get_rpm_names_versions,
 )
+from pulp_smash.tests.pulp2.rpm.utils import gen_yum_config_file
 from pulp_smash.tests.pulp2.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
 
 

pulp_smash/tests/pulp2/rpm/api_v2/utils.py
@@ -8,6 +8,8 @@ from os.path import basename
 from urllib.parse import urljoin
 from xml.etree import ElementTree
 
+from packaging.version import Version
+
 from pulp_smash import api, cli, exceptions, selectors, utils
 from pulp_smash.constants import RPM_NAMESPACES
 
@@ -421,3 +423,23 @@ def set_pulp_manage_rsync(cfg, boolean):
     cmd += ('--on',) if boolean else ('--off',)
     cmd += ('pulp_manage_rsync',)
     return client.run(cmd)
+
+
+def get_rpm_names_versions(cfg, repo):
+    """Get a dict of a repository's RPMs and their versions.
+
+    :param pulp_smash.config.PulpSmashConfig cfg: Information about a Pulp app.
+    :param repo: A dict of information about a repository.
+    :returns: The name and versions of each package in the repository, with the
+        versions sorted in ascending order. For example: ``{'walrus': ['0.71',
+        '5.21']}``.
+    """
+    rpms = utils.search_units(cfg, repo, {'type_ids': ['rpm']})
+    names_versions = {}
+    for rpm in rpms:
+        rpm_name = rpm['metadata']['name']
+        names_versions.setdefault(rpm_name, [])
+        names_versions[rpm_name].append(rpm['metadata']['version'])
+    for versions in names_versions.values():
+        versions.sort(key=Version)
+    return names_versions

pulp_smash/tests/pulp2/rpm/cli/test_copy_units.py
@@ -4,6 +4,8 @@ import subprocess
 import unittest
 from urllib.parse import urljoin
 
+from packaging.version import Version
+
 from pulp_smash import cli, config, constants, selectors, utils
 from pulp_smash.tests.pulp2.rpm.cli.utils import count_langpacks
 from pulp_smash.tests.pulp2.rpm.utils import (
@@ -11,7 +13,6 @@ from pulp_smash.tests.pulp2.rpm.utils import (
     check_issue_2620,
     check_issue_3104,
     gen_yum_config_file,
-    get_rpm_names_versions,
     set_up_module,
 )
 from pulp_smash.utils import is_root
@@ -106,7 +107,7 @@ class CopyTestCase(UtilsMixin, unittest.TestCase):
             'pulp-admin rpm repo copy rpm --from-repo-id {} --to-repo-id {} '
             '--str-eq name=chimpanzee'.format(_REPO_ID, repo_id).split()
         )
-        rpms = get_rpm_names_versions(cfg, repo_id)
+        rpms = _get_rpm_names_versions(cfg, repo_id)
         self.assertEqual(list(rpms.keys()), ['chimpanzee'])
         self.assertEqual(len(rpms['chimpanzee']), 1, rpms)
 
@@ -140,12 +141,12 @@ class CopyRecursiveTestCase(UtilsMixin, unittest.TestCase):
         )
 
         # Verify only one "walrus" unit has been copied
-        dst_rpms = get_rpm_names_versions(cfg, repo_id)
+        dst_rpms = _get_rpm_names_versions(cfg, repo_id)
         self.assertIn('walrus', dst_rpms)
         self.assertEqual(len(dst_rpms['walrus']), 1, dst_rpms)
 
         # Verify the version of the "walrus" unit
-        src_rpms = get_rpm_names_versions(cfg, _REPO_ID)
+        src_rpms = _get_rpm_names_versions(cfg, _REPO_ID)
         self.assertEqual(src_rpms['walrus'][-1], dst_rpms['walrus'][0])
 
 
@@ -221,7 +222,7 @@ class UpdateRpmTestCase(UtilsMixin, unittest.TestCase):
 
         # Pick an RPM with two versions.
         rpm_name = 'walrus'
-        rpm_versions = get_rpm_names_versions(cfg, _REPO_ID)[rpm_name]
+        rpm_versions = _get_rpm_names_versions(cfg, _REPO_ID)[rpm_name]
 
         # Copy the older RPM to the second repository, and publish it.
         self._copy_and_publish(cfg, rpm_name, rpm_versions[0], repo_id)
@@ -286,3 +287,31 @@ class UpdateRpmTestCase(UtilsMixin, unittest.TestCase):
         for stream in ('stdout', 'stderr'):
             with self.subTest(stream=stream):
                 self.assertNotIn('Task Failed', getattr(proc, stream))
+
+
+def _get_rpm_names_versions(cfg, repo_id):
+    """Get a dict of repo's RPMs with names as keys, mapping to version lists.
+
+    :param pulp_smash.config.PulpSmashConfig cfg: Information about a Pulp
+        application.
+    :param repo_id: A RPM repository ID.
+    :returns: The name and versions of each package in the repository, with the
+        versions sorted in ascending order. For example: ``{'walrus': ['0.71',
+        '5.21']}``.
+    """
+    keyword = 'Filename:'
+    completed_proc = cli.Client(cfg).run(
+        'pulp-admin rpm repo content rpm --repo-id {}'.format(repo_id).split()
+    )
+    names_versions = {}
+    for line in completed_proc.stdout.splitlines():
+        if keyword not in line:
+            continue
+        # e.g. 'Filename: my-walrus-0.71-1.noarch.rpm ' → ['my-walrus', '0.71']
+        filename_parts = line.lstrip(keyword).strip().split('-')[:-1]
+        name = '-'.join(filename_parts[:-1])
+        version = filename_parts[-1]
+        names_versions.setdefault(name, []).append(version)
+    for versions in names_versions.values():
+        versions.sort(key=Version)
+    return names_versions

pulp_smash/tests/pulp2/rpm/utils.py
@@ -144,23 +144,3 @@ def gen_yum_config_file(cfg, repositoryid, **kwargs):
             .format(section.getvalue(), '' if is_root(cfg) else 'sudo ', path)
         )
     return path
-
-
-def get_rpm_names_versions(cfg, repo):
-    """Get a dict of a repository's RPMs and their versions.
-
-    :param pulp_smash.config.PulpSmashConfig cfg: Information about a Pulp app.
-    :param repo: A dict of information about a repository.
-    :returns: The name and versions of each package in the repository, with the
-        versions sorted in ascending order. For example: ``{'walrus': ['0.71',
-        '5.21']}``.
-    """
-    rpms = utils.search_units(cfg, repo, {'type_ids': ['rpm']})
-    names_versions = {}
-    for rpm in rpms:
-        rpm_name = rpm['metadata']['name']
-        names_versions.setdefault(rpm_name, [])
-        names_versions[rpm_name].append(rpm['metadata']['version'])
-    for versions in names_versions.values():
-        versions.sort()
-    return names_versions