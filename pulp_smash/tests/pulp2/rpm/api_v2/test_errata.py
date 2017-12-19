e7d3e0d342ec02514714054ce3686b7fc7ea0afb
Jeremy Audet <ichimonji10@gmail.com>
2017-12-19 13:46:14 -0500

    Refactor new test case for large updateinfo.xml
    Use for loops to deduplicate redundant code and apply assertions to more
    responses.

--- pulp_smash/tests/pulp2/rpm/api_v2/test_errata.py
+++ pulp_smash/tests/pulp2/rpm/api_v2/test_errata.py
@@ -2,7 +2,7 @@
 import unittest
 from urllib.parse import urljoin
 
-from pulp_smash import api, cli, config, utils
+from pulp_smash import api, cli, config, selectors, utils
 from pulp_smash.constants import RPM_LARGE_UPDATEINFO, RPM_UNSIGNED_FEED_URL
 from pulp_smash.tests.pulp2.constants import REPOSITORY_PATH
 from pulp_smash.tests.pulp2.rpm.api_v2.utils import (
@@ -115,36 +115,35 @@ class LargePackageListTestCase(unittest.TestCase):
 
         This test targets the following issue:
 
+        * `Pulp #2681 <https://pulp.plan.io/issues/2681>`_
         * `Pulp Smash #656 <https://github.com/PulpQE/pulp-smash/issues/656>`_
 
         Do the following:
 
-        1. Create two repositories, with a feed URL that contains a large
-           errata package list.
-        2. Sync both repositories.
-        3. Assert that sync of second repository was finished without any
-           error.
+        1. Create two repositories, each with a feed URL of
+           :data:`pulp_smash.constants.RPM_LARGE_UPDATEINFO`.
+        2. Sync both repositories. Assert that each sync finishes without
+           errors.
         """
-        repos = []
         cfg = config.get_config()
+        if selectors.bug_is_untestable(2681, cfg.pulp_version):
+            self.skipTest('https://pulp.plan.io/issues/2681')
+        repos = []
         client = api.Client(cfg, api.json_handler)
-        # Create first repository.
-        body = gen_repo()
-        body['importer_config']['feed'] = RPM_LARGE_UPDATEINFO
-        repo = client.post(REPOSITORY_PATH, body)
-        self.addCleanup(client.delete, repo['_href'])
-        repos.append(repo)
-        # Create second repository.
-        body = gen_repo()
-        body['importer_config']['feed'] = RPM_LARGE_UPDATEINFO
-        repo = client.post(REPOSITORY_PATH, body)
-        self.addCleanup(client.delete, repo['_href'])
-        repos.append(repo)
-        # Sync both repositories.
-        utils.sync_repo(cfg, repos[0])
-        report = utils.sync_repo(cfg, repos[1])
-        tasks = tuple(api.poll_spawned_tasks(cfg, report.json()))
-        for i, task in enumerate(tasks):
-            with self.subTest(i=i):
-                error_details = task['progress_report']['yum_importer']['content']['error_details']  # noqa pylint:disable=line-too-long
-                self.assertEqual(error_details, [], task)
+
+        # Create repositories.
+        for _ in range(2):
+            body = gen_repo()
+            body['importer_config']['feed'] = RPM_LARGE_UPDATEINFO
+            repo = client.post(REPOSITORY_PATH, body)
+            self.addCleanup(client.delete, repo['_href'])
+            repos.append(repo)
+
+        # Sync repositories.
+        for repo in repos:
+            report = utils.sync_repo(cfg, repo)
+            tasks = tuple(api.poll_spawned_tasks(cfg, report.json()))
+            for i, task in enumerate(tasks):
+                with self.subTest(i=i):
+                    error_details = task['progress_report']['yum_importer']['content']['error_details']  # noqa pylint:disable=line-too-long
+                    self.assertEqual(error_details, [], task)
