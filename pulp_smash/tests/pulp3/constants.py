064f59a35b65a03f7ce85abbf5b5f0863529fdee
Jeremy Audet <ichimonji10@gmail.com>
2018-01-25 13:52:26 -0500

    Test each combo of download policy and sync mode
    Expand the recently added `SyncFileRepoTestCase`. Currently, this test
    case creates an importer with a random download policy and a random sync
    mode. With this commit, the test case will instead iterate through each
    possible pairing of download policy and sync mode.
    
    This change in test logic uncovered a bug, wherein not all of the
    download policies are implemented for the file plugin.
    
    See: https://pulp.plan.io/issues/3320

--- pulp_smash/tests/pulp3/constants.py
+++ pulp_smash/tests/pulp3/constants.py
@@ -14,8 +14,18 @@ FILE_IMPORTER_PATH = urljoin(BASE_IMPORTER_PATH, 'file/')
 FILE_PUBLISHER_PATH = urljoin(BASE_PUBLISHER_PATH, 'file/')
 
 IMPORTER_DOWN_POLICY = {'background', 'immediate', 'on_demand'}
+"""Download policies for an importer.
+
+See `pulpcore.app.models.Importer
+<https://docs.pulpproject.org/en/3.0/nightly/contributing/platform_api/app/models.html#pulpcore.app.models.Importer>`_.
+"""
 
 IMPORTER_SYNC_MODE = {'additive', 'mirror'}
+"""Sync modes for an importer.
+
+See `pulpcore.app.models.Importer
+<https://docs.pulpproject.org/en/3.0/nightly/contributing/platform_api/app/models.html#pulpcore.app.models.Importer>`_.
+"""
 
 JWT_PATH = urljoin(BASE_PATH, 'jwt/')
 
--- pulp_smash/tests/pulp3/file/api_v3/test_sync.py
+++ pulp_smash/tests/pulp3/file/api_v3/test_sync.py
@@ -1,11 +1,17 @@
 # coding=utf-8
 """Tests that sync file plugin repositories."""
 import unittest
+from itertools import product
 from urllib.parse import urljoin
 
-from pulp_smash import api, config
+from pulp_smash import api, config, selectors
 from pulp_smash.constants import FILE_FEED_URL
-from pulp_smash.tests.pulp3.constants import FILE_IMPORTER_PATH, REPO_PATH
+from pulp_smash.tests.pulp3.constants import (
+    FILE_IMPORTER_PATH,
+    IMPORTER_DOWN_POLICY,
+    IMPORTER_SYNC_MODE,
+    REPO_PATH,
+)
 from pulp_smash.tests.pulp3.file.api_v3.utils import gen_importer
 from pulp_smash.tests.pulp3.file.utils import set_up_module as setUpModule # noqa pylint:disable=unused-import
 from pulp_smash.tests.pulp3.pulpcore.utils import gen_repo
@@ -15,13 +21,35 @@ from pulp_smash.tests.pulp3.utils import get_auth, sync_repo
 class SyncFileRepoTestCase(unittest.TestCase):
     """Sync repositories with the file plugin."""
 
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.cfg = config.get_config()
+
     def test_all(self):
-        """Sync repositories with the file plugin..
+        """Call :meth:`do_test` with varying arguments.
+
+        Call :meth:`do_test` with each possible pairing of
+        :data:`pulp_smash.tests.pulp3.constants.IMPORTER_DOWN_POLICY` and
+        :data:`pulp_smash.tests.pulp3.constants.IMPORTER_SYNC_MODE`. If `Pulp
+        #3320`_ affects Pulp, then the ``background`` and ``on_demand``
+        download policies are omitted from the test matrix.
+
+        .. _Pulp #3320: https://pulp.plan.io/issues/3320
+        """
+        importer_down_policy = IMPORTER_DOWN_POLICY
+        if selectors.bug_is_untestable(3320, self.cfg):
+            importer_down_policy -= {'background', 'on_demand'}
+        for pair in product(importer_down_policy, IMPORTER_SYNC_MODE):
+            with self.subTest(pair=pair):
+                self.do_test(*pair)
+
+    def do_test(self, download_policy, sync_mode):
+        """Sync repositories with the file plugin.
 
         In order to sync a repository an importer has to be associated within
-        this repository.
-        When a repository is created this version field is set as None. After a
-        sync the repository version is updated.
+        this repository. When a repository is created this version field is set
+        as None. After a sync the repository version is updated.
 
         Do the following:
 
@@ -32,25 +60,28 @@ class SyncFileRepoTestCase(unittest.TestCase):
         5. Sync the importer one more time.
         6. Assert that repository version is different from the previous one.
 
+        :param download_policy: The download policy for the importer.
+        :param sync_mode: The download policy for the importer.
         """
-        cfg = config.get_config()
-        client = api.Client(cfg, api.json_handler)
+        client = api.Client(self.cfg, api.json_handler)
         client.request_kwargs['auth'] = get_auth()
         repo = client.post(REPO_PATH, gen_repo())
         self.addCleanup(client.delete, repo['_href'])
         body = gen_importer(repo)
+        body['download_policy'] = download_policy
         body['feed_url'] = urljoin(FILE_FEED_URL, 'PULP_MANIFEST')
+        body['sync_mode'] = sync_mode
         importer = client.post(FILE_IMPORTER_PATH, body)
         self.addCleanup(client.delete, importer['_href'])
 
         # Sync the repository.
-        self.assertEqual(repo['_latest_version_href'], None)
-        sync_repo(cfg, importer)
+        self.assertIsNone(repo['_latest_version_href'])
+        sync_repo(self.cfg, importer)
         repo = client.get(repo['_href'])
-        self.assertNotEqual(repo['_latest_version_href'], None)
+        self.assertIsNotNone(repo['_latest_version_href'])
 
         # Sync the repository again.
         latest_version_href = repo['_latest_version_href']
-        sync_repo(cfg, importer)
+        sync_repo(self.cfg, importer)
         repo = client.get(repo['_href'])
         self.assertNotEqual(latest_version_href, repo['_latest_version_href'])
