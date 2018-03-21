f4f9f14a90c7073befac2e7647938cb0bb99f62d
Jeremy Audet <jerebear@protonmail.com>
2018-03-21 14:09:13 -0400

    Drop warning about base_path trailing slash
    `pulp_smash.tests.pulp3.pulpcore.utils.gen_distribution()` returns a
    dict that can be used to generate a distribution. A `base_path` is a
    required attribute for a distribution, and as such, this function must
    answer the following question:
    
    > May a distribution's `base_path` have a trailing slash?
    
    Pulp issue 3412 asks this question more formally. It has been closed,
    and the answer is "no, a distribution's `base_path` may not have a
    trailing slash."
    
    Drop a warning about this issue from `gen_distribution()`.

--- pulp_smash/tests/pulp3/pulpcore/utils.py
+++ pulp_smash/tests/pulp3/pulpcore/utils.py
@@ -1,9 +1,12 @@
 # coding=utf-8
 """Utilities for Pulpcore tests."""
-import warnings
-
-from pulp_smash import config, selectors, utils
+from pulp_smash import utils
 from pulp_smash.tests.pulp3 import utils as pulp3_utils
+
+
+def gen_distribution():
+    """Return a semi-random dict for use in creating a distribution."""
+    return {'base_path': utils.uuid4(), 'name': utils.uuid4()}
 
 
 def gen_repo():
@@ -15,16 +18,3 @@ def set_up_module():
     """Skip tests Pulp 3 isn't under test or if pulpcore isn't installed."""
     pulp3_utils.require_pulp_3()
     pulp3_utils.require_pulp_plugins({'pulpcore'})
-
-
-def gen_distribution():
-    """Return a semi-random dict for use in creating a distribution."""
-    if selectors.bug_is_testable(3412, config.get_config().pulp_version):
-        warnings.warn(
-            'The base_path field may be malformed. See: '
-            'https://pulp.plan.io/issues/3412'
-        )
-    return {
-        'base_path': utils.uuid4(),
-        'name': utils.uuid4()
-    }
