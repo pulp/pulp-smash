54759d562bfb1ee8226f7e00c098c37dcf2e85e9
Dennis Kliban <dkliban@redhat.com>
2018-04-20 15:35:10 -0400

    removed onlinewq

--- pulp_smash/tests/pulp3/pulpcore/api_v3/test_workers.py
+++ pulp_smash/tests/pulp3/pulpcore/api_v3/test_workers.py
@@ -67,7 +67,6 @@ class WorkersTestCase(unittest.TestCase, utils.SmokeTest):
         page = self.client.get(WORKER_PATH, params={
             'last_heartbeat__gte': self.worker['last_heartbeat'],
             'name': self.worker['name'],
-            'online': self.worker['online'],
         })
         self.assertEqual(len(page['results']), 1)
         for key, val in self.worker.items():
