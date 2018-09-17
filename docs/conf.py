1fab4b6560be61bf4f3a0740873674a134da2c19
kersom <kersom@gmail.com>
2018-09-17 10:56:01 -0400

    Update documentation
    Include a link to PEP-8 on code standards.
    
    Update Sphinx `conf.py`. To reflect the new changes of `autodoc_default_flags`
    that was deprecated on Sphinx 1.8. Update to use `autodoc_default_options`.

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 exclude_patterns = ['_build']
 nitpicky = True
-autodoc_default_flags = ['members']
-
+autodoc_default_options = {'members': None, 'undoc-members': None}
 
 # Format-Specific Options -----------------------------------------------------
 htmlhelp_basename = 'PulpSmashdoc'
