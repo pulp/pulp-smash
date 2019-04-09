ceae0f60c58d8d07bcd62f193226ac582baf5e11
koliveir <koliveir@redhat.com>
2019-04-09 09:51:14 -0400

    Release version 1!0.4.0
    Shortlog of commits since last release:
    
        Brian Bouterse (2):
              Updates pulp-smash for repo layout changes
              Introduce BASE_CONTENT_GUARD_PATH
    
        Bruno Rocha (8):
              Add `download_content_unit` helper function for Pulp 3
              Adds `RegistryClient` to talk woth container registries.
              Fic certguard endpoint
              add kwargs to download_content_unit function
              Add `ensure_teardownclass` context manager to pulp_smash.utils
              Add api.task_handler and related Client.using_handler
              Add 'pulp-smash shell' command.
              Lazy load the cli.Client machine
    
        Daniel Alley (1):
              Update get_artifact_paths() to be more correct
    
        koliveir (4):
              Remove constant USER_PATH
              Add lazy download policies constant
              Overhaul CI and apply code format
              Update documentation about task_handler

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "Intended Audience :: Developers",
         (
             "License :: OSI Approved :: GNU General Public License v3 or later"
-            "(GPLv3+)"
+            " (GPLv3+)"
         ),
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
